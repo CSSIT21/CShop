41e8e57c316340d61b8254d17c97bcc1cbb8e821
Apisit Maneerat <70818990+Mixko50@users.noreply.github.com>
2021-10-21 23:17:50 +0700

    [Update] Deleted unused variables

--- frontend/src/_home/pages/Home.js
+++ frontend/src/_home/pages/Home.js
@@ -1,16 +1,14 @@
-import { useLocation } from "react-router";
-import { For, useQuery } from "../../common/utils";
+import { For } from "../../common/utils";
 
-const HomePage = props => {
-    const { name, hello } = useQuery();
-    return <div>
-        hello tihs is HomePage
-        <ul>
-            <For each={[1,2,3,4,5]}>
-                {(item) => <li>{item}</li>}
-            </For>
-        </ul>
-    </div>;
+const HomePage = (props) => {
+    return (
+        <div>
+            hello tihs is HomePage
+            <ul>
+                <For each={[1, 2, 3, 4, 5]}>{(item) => <li>{item}</li>}</For>
+            </ul>
+        </div>
+    );
 };
 
 export default HomePage;
