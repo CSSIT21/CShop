de48998392bc47536cec10e33a8eaac7515cfda1
Apisit Maneerat <70818990+Mixko50@users.noreply.github.com>
2021-10-21 15:09:40 +0700

    [Add] _manage

---
+++ frontend/src/_manage/pages/ManageAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MiniDrawer from '../components/Sidebar';
+
+const ManageAccount = () => {
+	return (
+		<div>
+			<MiniDrawer />
+		</div>
+	);
+};
+
+export default ManageAccount;
