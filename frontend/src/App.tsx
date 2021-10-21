69bf2fb42cddb935a5f8f5695e03d8b2ecce1016
Apisit Maneerat <70818990+Mixko50@users.noreply.github.com>
2021-10-21 14:36:38 +0700

    [Add] _manage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,22 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { ManageAccount } from "./_manage/page/ManageAccount";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>count is: {count}</button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/about">
+                            <ManageAccount />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/_manage/page/ManageAccount.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+export const ManageAccount = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    );
+};
