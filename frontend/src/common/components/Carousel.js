419651e3dcce420a7bba09db9d3c3eaabec1de1f
Floaty_KT <69688279+FloatKasemtan@users.noreply.github.com>
2021-10-22 21:16:13 +0700

    [UPDATE] Common Carousel

---
+++ frontend/src/common/components/Carousel.js
@@ -0,0 +1,32 @@
+import CarouselBase from "./CarouselBase";
+
+const Carousel = ({
+  items = [],
+  itemsPerRow = 1,
+  rows = 1,
+  gap = 10,
+  loop = false,
+  pageState = 0,
+  setPageState = void 0,
+  children,
+  ...rest
+}) => {
+  return (
+    <CarouselBase
+      cols={itemsPerRow}
+      rows={rows}
+      gap={gap}
+      loop={loop}
+      pageState={pageState}
+      setPageState={setPageState}
+      hideArrow
+      {...rest}
+    >
+      {items.map((item, idx) => (
+        <CarouselBase.Item key={idx}>{children(item, idx)}</CarouselBase.Item>
+      ))}
+    </CarouselBase>
+  );
+};
+
+export default Carousel;
