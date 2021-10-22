069d7fa73ef3ddc3335fec9c717b62a7b2fc15c4
Ms.MONTHARA KIATRUANGCHAI <monthara.k@mail.kmutt.ac.th>
2021-10-22 22:53:43 +0700

    [UPDATE] Navbar

--- frontend/src/common/components/Carousel.js
+++ frontend/src/common/components/Carousel.js
@@ -1,20 +1,4 @@
-import { For } from "../utils/index";
-import { useState } from "react";
-import CarouselBase from "./CarouselBase";
-import CustomDot from "./CarouselBase/CustomDot";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-
-import Box from "@mui/material/Box";
-
-const style = {
-  width: "80vw",
-  margin: "0px auto",
-};
+import CarouselBase from "./CarouselBase"; ฃ
 
 const Carousel = ({
   items = [],
@@ -27,32 +11,21 @@ const Carousel = ({
   children,
   ...rest
 }) => {
-  const itemsNumber = items.length;
-  const totalPage = Math.ceil(itemsNumber / itemsPerRow);
-
   return (
-    <Box sx={style}>
-      <CarouselBase
-        cols={itemsPerRow}
-        rows={rows}
-        gap={gap}
-        loop={loop}
-        pageState={pageState}
-        setPageState={setPageState}
-        hideArrow
-        {...rest}
-      >
-        {items.map((item, idx) => (
-          <CarouselBase.Item key={idx}>{children(item, idx)}</CarouselBase.Item>
-        ))}
-      </CarouselBase>
-      <CustomDot
-        width="10%"
-        currentPage={pageState}
-        totalPage={totalPage}
-        setPageState={setPageState}
-      />
-    </Box>
+    <CarouselBase
+      cols={itemsPerRow}
+      rows={rows}
+      gap={gap}
+      loop={loop}
+      pageState={pageState}
+      setPageState={setPageState}
+      hideArrow
+      {...rest}
+    >
+      {items.map((item, idx) => (
+        <CarouselBase.Item key={idx}>{children(item, idx)}</CarouselBase.Item>
+      ))}
+    </CarouselBase>
   );
 };
 
