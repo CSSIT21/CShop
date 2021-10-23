4e10b02d669f36695a05b662b48a6ddbfb17a2be
Ms.THANAWAN PANAPONGPAISAN <63130500211@st.sit.kmutt.ac.th>
2021-10-23 16:23:39 +0700

    filter-price/rate

--- frontend/src/_category/pages/Category.js
+++ frontend/src/_category/pages/Category.js
@@ -1,7 +1,18 @@
-const CategoryPage = props => {
-    return <div>
-        Hello, this is CategoryPage
-    </div>;
-};
+import React, { Component } from 'react';
+import './Category.css'
+import PriceRange from './price-filter/PriceRange';
+import RateStar from './rate-filter/RateStar';
 
-export default CategoryPage;
+
+export default class CategoryPage extends Component {
+  render() {
+    return (
+      <div className='filter'>
+        <img src='https://static.thenounproject.com/png/40256-200.png' />
+        <h5>Filter</h5>
+        <PriceRange />
+        <RateStar/>
+      </div>
+    );
+  }
+}
