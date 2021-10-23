eea6fbe498bde8c54472709b51d98350f4f003d4
Apisit Maneerat <70818990+Mixko50@users.noreply.github.com>
2021-10-23 13:39:45 +0700

    [Update] CButton (#9)

--- frontend/src/_manage/pages/Sellers.js
+++ frontend/src/_manage/pages/Sellers.js
@@ -1,10 +1,14 @@
-import { Box } from '@mui/system';
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 const ManageSellerAccountPage = () => {
-	return (
-		<div>
-			<Box>Seller</Box>
-		</div>
-	);
+    return (
+        <div>
+            <Box>Seller</Box>
+            <Typography variant="h3">
+                Almost before we knew it, we had left the ground.
+            </Typography>
+        </div>
+    );
 };
 
 export default ManageSellerAccountPage;
