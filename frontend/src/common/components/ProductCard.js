33976b0e2790c928c06736725e364b6626bbe2f4
Ms.MONTHARA KIATRUANGCHAI <monthara.k@mail.kmutt.ac.th>
2021-10-25 02:59:51 +0700

    [UPDATE] Product Card

--- frontend/src/common/components/ProductCard.js
+++ frontend/src/common/components/ProductCard.js
@@ -1,62 +1,34 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import BottomCardContent from "./ProductCardBase/BottomCardContent";
-import TopCardContent from "./ProductCardBase/TopCardContent";
-import Link from "@mui/material/Link";
+import ProductMedia from "./ProductCardBase/ProductMedia";
+import ProductContent from "./ProductCardBase/ProductContent";
 
-const ProductCard = ({
-  image = "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
-  title = "Product name",
-  price = 1000,
-  status = "sold out",
-  favourite = false,
-  margin = 10,
-  to,
-  ...rest
-}) => {
-  const onFavourite = (idx) => {
-    setItems((items) => {
-      items[idx].favourite = !items[idx].favourite;
-      return [...items];
-    });
-  };
+const cardStyle = {
+  minWidth: 200,
+  maxWidth: 210,
+  padding: '8px',
 
+  borderRadius: "15px",
+  border: 'none',
+  transition: "all ease 0.125s",
+
+  "&:hover": {
+    transform: "scale(1.02)",
+  },
+};
+
+const ProductCard = ({ product, to, onFavourite = () => { } }) => {
+  console.log(product);
   return (
-    <Card
-      sx={{
-        height: "285px",
-        maxWidth: "250px",
-        borderRadius: "15px",
-        margin: { margin },
-        "&:hover": {
-          transform: "scale(1.02)",
-        },
-        transition: "all ease 0.125s",
-      }}
-      {...rest}
-    >
-      <CardMedia
-        component="img"
-        sx={{
-          margin: "10px 10px",
-          maxWidth: "230px",
-          height: "180px",
-          borderRadius: "15px",
-        }}
-        image={image}
-        alt={title}
-      />
-      <CardContent>
-        <TopCardContent title={title} price={price} />
-        <BottomCardContent
-          status={status}
-          favourite={favourite}
-          onFavourite={onFavourite}
-        />
-      </CardContent>
-    </Card>
+    <Link to={to}>
+      <Card
+        variant="outlined"
+        sx={cardStyle}
+      >
+        <ProductMedia image={product.image} title={product.title} />
+        <ProductContent product={product} onFavourite={onFavourite} />
+      </Card>
+    </Link>
   );
 };
 
