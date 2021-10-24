f1118b62fc5494a772140ce3ff1d92453341e73c
Yok-Thanawan <thanawan.li@mail.kmutt.ac.th>
2021-10-24 11:21:01 +0700

    [update]ProductDescription&ShopDetail

--- frontend/src/_product/sections/ProductDescription.js
+++ frontend/src/_product/sections/ProductDescription.js
@@ -1,11 +1,63 @@
 import React from "react";
+import ExpandLessRoundedIcon from '@mui/icons-material/ExpandLessRounded';
+import { Accordion, AccordionDetails, AccordionSummary, Divider, Fade, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import Box from "@mui/material/Box";
 
 const ProductDescription = (props) => {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const handleClick = () => {
-    setOpen(open);
+    setOpen(!open);
   };
-  return <div>Product Detail</div>;
+  
+  const classes = useStyles();
+ 
+  return ( 
+    <>
+      <Box
+        sx={{
+          padding: "50px 0px",
+        }}
+      >
+        <Accordion
+          className={classes.accordion}
+          TransitionComponent={Fade}
+          TransitionProps={{ onExited: () => setOpen(false), timeout: 700 }}
+          disableGutters
+        >
+          <AccordionSummary
+            expandIcon={<ExpandLessRoundedIcon />}
+            aria-controls="expand-detail"
+            id="expand-detail"
+            onClick={handleClick}
+          >
+            <Box className={classes.box}>
+              <Typography sx={{ fontWeight: 600, fontSize: "30px" }}>
+                Product Detail
+              </Typography>
+            </Box>
+          </AccordionSummary>
+          <AccordionDetails>{open && <typography>test</typography>}  
+           </AccordionDetails>
+        </Accordion>
+        {!open && (
+          <Fade in={!open} timeout={500}>
+            <Divider />
+          </Fade>
+        )}
+      </Box>
+    </>
+  );
 };
-
+const useStyles = makeStyles({
+  box: {
+    display: "flex",
+    alignItems: "center",
+    padding: "0px 0px 50px 0px",
+    margin: "0px",
+  },
+  accordion: {
+    boxShadow: "none !important",
+  },
+});
 export default ProductDescription;
--- frontend/src/_product/sections/ShopDetails.js
+++ frontend/src/_product/sections/ShopDetails.js
@@ -1,5 +1,38 @@
+import React from "react";
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 const ShopDetails = (props) => {
-  return <div>Hello, this is ShopDetails</div>;
+  return (
+  <div classname="shopdetail">
+    <div className="left-part">
+      <Avatar alt="Shop" src="/static/images/avatar/1.jpg" sx={{ width: 95, height: 95 }} />
+      <div className="shopName">
+        Shop Name  
+      </div>
+      <div className="active">
+        Active 19 minutes ago
+      </div> 
+      <div className="button">
+        Chat now / Go to shop
+      </div>
+    </div>
+    <div className="right-part">
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'left' }}>
+        <Typography sx={{ minWidth: 100 }}>Rating</Typography>
+        <Typography sx={{ minWidth: 100 }}>4.7</Typography>
+        <Typography sx={{ minWidth: 100 }}>Followers</Typography>
+        <Typography sx={{ minWidth: 100 }}>7.4k</Typography>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'left' }}>
+        <Typography sx={{ minWidth: 100 }}>Products</Typography>
+        <Typography sx={{ minWidth: 100 }}>6666</Typography>
+        <Typography sx={{ minWidth: 100 }}>Joined</Typography>
+        <Typography sx={{ minWidth: 100 }}>4 years ago</Typography>
+      </Box>
+    </div>
+  </div>);
 };
 
 export default ShopDetails;
