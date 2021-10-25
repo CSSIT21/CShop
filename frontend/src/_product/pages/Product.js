17a9837c683d69138bd2dce2390a194490d89ace
Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
2021-10-25 15:35:29 +0700

    [ADD] Carousel

--- frontend/src/_product/pages/Product.js
+++ frontend/src/_product/pages/Product.js
@@ -6,7 +6,77 @@ import ProductRating from "../sections/ProductRating";
 import ProductSuggestion from "../sections/ProductSuggestion";
 import { Box } from "@mui/material";
 
+const Bestseller1 =
+  "https://hbr.org/resources/images/article_assets/2019/11/Nov19_14_sb10067951dd-001.jpg";
+
 const ProductPage = (props) => {
+  const itemsData = [
+    {
+      id: 0,
+      title: "Cheese Pizza very อร่อย มากๆๆๆ",
+      price: "500",
+      status: "Hot sale",
+      favourite: true,
+      image: Bestseller1,
+    },
+    {
+      id: 1,
+      title: "Cheese Pizza",
+      price: "500",
+      status: "Hot sale",
+      favourite: false,
+      image: Bestseller1,
+    },
+    {
+      id: 2,
+      title: "Cheese Pizza",
+      price: "500",
+      status: "Hot sale",
+      favourite: false,
+      image: Bestseller1,
+    },
+    {
+      id: 3,
+      title: "Cheese Pizza",
+      price: "500",
+      status: "Hot sale",
+      favourite: false,
+      image: Bestseller1,
+    },
+    {
+      id: 4,
+      title: "Cheese Pizza",
+      price: "500",
+      status: "Hot sale",
+      favourite: true,
+      image: Bestseller1,
+    },
+    {
+      id: 5,
+      title: "Cheese Pizza",
+      price: "500",
+      status: "Hot sale",
+      favourite: true,
+      image: Bestseller1,
+    },
+    {
+      id: 6,
+      title: "Cheese Pizza",
+      price: "500",
+      status: "Hot sale",
+      favourite: false,
+      image: Bestseller1,
+    },
+  ];
+  const [items, setItems] = useState(itemsData);
+  const onFavourite = (e, idx) => {
+    e.preventDefault();
+    setItems((items) => {
+      items[idx].favourite = !items[idx].favourite;
+      return [...items];
+    });
+  };
+
   return (
     <Box
       sx={{
@@ -22,7 +92,7 @@ const ProductPage = (props) => {
       >
         <ProductDetails />
         <ShopDetails />
-        <ProductSuggestion />
+        <ProductSuggestion bestsellerItems={items} onFavourite={onFavourite} />
         <ProductDescription />
         <ProductRating />
       </Box>
--- frontend/src/_product/sections/ProductSuggestion.js
+++ frontend/src/_product/sections/ProductSuggestion.js
@@ -1,148 +1,89 @@
+import React from "react";
+import { useState } from "react";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
-import CustomDot from "../../common/components/CarouselBase/CustomDot";
-import Carousel from "../../common/components/Carousel";
-import ProductCard from "../../common/components/ProductCard";
-import CarouselButton from "../../common/components/CarouselButton";
 import { makeStyles } from "@mui/styles";
 import { Typography } from "@mui/material";
+import Carousel from "../../common/components/Carousel";
+import CustomDot from "../../common/components/CarouselBase/CustomDot";
+import ProductCard from "../../common/components/ProductCard";
+import CarouselButton from "../../common/components/CarouselButton";
 
-const ProductSuggestion = ({}) => {
-  const classes = useStyles();
+const useStyles = makeStyles({
+  bestsellerWrapper: {
+    width: "100%",
+    margin: "20px 0",
+  },
 
-  const itemsAxios = [
-    {
-      id: 0,
-      title: "item1",
-      favourite: false,
-      description: "werewrwerwe",
-    },
-    {
-      id: 1,
-      title: "item2",
-      favourite: true,
-      description: "pytuiui",
-    },
-    {
-      id: 2,
-      title: "item3",
-      favourite: false,
-      description: "pytuiui",
-    },
-    {
-      id: 3,
-      title: "item4",
-      favourite: true,
-      description: "pytuiui",
-    },
-    {
-      id: 4,
-      title: "item5",
-      favourite: false,
-      description: "pytuiui",
-    },
-    {
-      id: 5,
-      title: "item6",
-      favourite: false,
-      description: "pytuiui",
-    },
-    {
-      id: 6,
-      title: "item7",
-      favourite: false,
-      description: "pytuiui",
-    },
-    {
-      id: 7,
-      title: "item8",
-      favourite: false,
-      description: "pytuiui",
-    },
-    {
-      id: 8,
-      title: "item9",
-      favourite: false,
-      description: "pytuiui",
-    },
-    {
-      id: 9,
-      title: "item10",
-      favourite: false,
-      description: "pytuiui",
-    },
-    {
-      id: 10,
-      title: "item11",
-      favourite: false,
-      description: "pytuiui",
-    },
-  ];
+  bestsellerContent: {
+    maxWidth: "1000px",
+    margin: "0 auto",
+    padding: "40px 80px",
 
-  const [items, setItems] = useState(itemsAxios);
+    backgroundColor: "#FDF4DD",
+    borderRadius: "20px",
+    marginBottom: "40px",
+  },
+
+  bestsellerHeader: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginBottom: "25px",
+  },
+});
+
+const ProductSuggestion = ({ bestsellerItems, onFavourite }) => {
+  const [items, setItems] = useState(bestsellerItems);
   const [page, setPage] = useState(0);
-
-  const onFavouriteHandler = (idx) => {
-    setItems((items) => {
-      items[idx].favourite = !items[idx].favourite;
-      return [...items];
-    });
-  };
+  const classes = useStyles();
+  const itemsPerRow = 4;
+  const totalPage = Math.ceil(items.length / itemsPerRow);
 
   return (
-    <Box sx={{ margin: "50px 0px" }}>
-      <Box className={classes.frame}>
-        <Box className={classes.container}>
-          <Typography sx={{ fontSize: "30px", fontWeight: 600 }}>
-            Suggestions
+    <Box className={classes.bestsellerWrapper}>
+      <Box className={classes.bestsellerContent}>
+        <Box className={classes.bestsellerHeader}>
+          <Typography
+            component="span"
+            color="#FD6637"
+            fontSize="30px"
+            fontWeight="bold"
+          >
+            Best Seller
           </Typography>
           <CarouselButton
             pageHandle={setPage}
             currentPage={page}
-            totalPage={Math.ceil(items.length / 4)}
+            totalPage={totalPage}
           />
         </Box>
-        <Carousel
-          items={items}
-          pageState={page}
-          setPageState={setPage}
-          loop={true}
-          itemsPerRow={4}
-        >
-          {(item, idx) => (
-            <ProductCard
-              title={item.title}
-              favourite={item.favourite}
-              onFavourite={() => {
-                onFavouriteHandler(idx);
-              }}
-              to={`/product/${item.id}`}
-            />
-          )}
-        </Carousel>
-        <CustomDot
-          currentPage={page}
-          totalPage={Math.ceil(items.length / 4)}
-          setPageState={setPage}
-          width="75px"
-        />
+
+        <Box className={classes.bestsellerCarousel}>
+          <Carousel
+            items={items}
+            pageState={page}
+            setPageState={setPage}
+            itemsPerRow={4}
+          >
+            {(item, idx) => (
+              <ProductCard
+                product={item}
+                onFavourite={onFavourite}
+                to="/product/1"
+                key={item.id}
+              />
+            )}
+          </Carousel>
+        </Box>
       </Box>
+
+      <CustomDot
+        width={50}
+        setPageState={setPage}
+        currentPage={page}
+        totalPage={totalPage}
+      />
     </Box>
   );
 };
-
-const useStyles = makeStyles({
-  frame: {
-    width: "100%",
-    height: "483px",
-    borderRadius: "20px",
-    background: "rgba(239, 239, 241, 0.7)",
-  },
-  container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-});
 
 export default ProductSuggestion;
