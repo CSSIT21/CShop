001fe3172044c5b02910c7e42df6eceb085565d1
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-10-25 21:06:42 +0700

    [ADD] useStateFlush hook

--- frontend/src/common/hooks/index.js
+++ frontend/src/common/hooks/index.js
@@ -1,21 +1,44 @@
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import authState from '../store/authState';
+import { isArray, isFunc, isObj } from '../utils';
+import { useCallback } from 'react';
 
-export const useAtomic = (init=null,flush=false) => {
+export const useStateFlush = (init=null,flush=false) => {
     const [state, setState] = useState(init);
 
-    function setAtomic(callback){
-        if(typeof callback === 'function'){
-            var newState = callback(state);
-            if(flush){
-                if(typeof newState === 'object')
-                    setState();
+    function setStateFlush(callback){
+        var newState = isFunc(callback) ? callback(state) : callback;
+        if(flush){
+            if(isObj(newState))
+                return setState({...newState});
+            if(isArray(newState))
+                return setState([...newState]);
+        }else{
+            if(isObj(newState)){
+                const cloneState = {...state};
+                Object.assign(cloneState, newState);
+                return setState({...cloneState});
             }
-            if(typeof newState)
-
-            return setState()
         }
-
+        return setState(newState);
     }
 
-    return [state,setAtomic];
+    return [state,setStateFlush];
+};
+
+export const useInput = (init="") => {
+    const [state, setState] = useState(init);
+    return {
+        value: state,
+        onInput: e => setState(e.target.value),
+    }
+}
+
+export const useForm = (init={}) => {
+    const form = {};
+    for(var prop in init){
+        form[prop] = init[prop].value;
+    }
+    return form;
 };
