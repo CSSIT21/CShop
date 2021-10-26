23a9da73bdc3c99a8bc1a986700c2538a5fa8bbc
janette-wong <janettewongprasith@gmail.com>
2021-10-27 02:06:41 +0700

    [UPDATE] all

--- frontend/src/_home/pages/Favourite.js
+++ frontend/src/_home/pages/Favourite.js
@@ -1,7 +1,72 @@
+import { useState } from "react";
+import {makeStyles} from "@mui/styles";
+import { Box } from "@mui/system";
+import { Typography, Grid} from '@mui/material';
+import { Favorite as FavoriteIcon } from '@mui/icons-material';
+import ProductCard from "~/common/components/ProductCard";
+import { For } from "~/common/utils";
+import fakeProducts from "~/common/faker/fakeProducts";
+
+const useStyles = makeStyles({
+    favoritepageWrapper: {
+        width: '100%',
+        boxSizing: 'border-box',
+        padding:'50px 200px',
+
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        backgroundColor: '#FDF4DD',
+    },
+    favoriteHeader: {
+       display:'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        marginBottom:'50px',
+        
+        
+
+    },
+});
+
 const FavouritePage = props => {
-    return <div>
-        This is FavouritePage
-    </div>;
+    const classes = useStyles();
+
+    const [page, setPage] = useState(0);
+    const [products, setProducts] = useState(fakeProducts);
+
+    const onFavourite = (index) => {
+        setProducts((products) => {
+            const target = products[index];
+            target.favourite = !target.favourite;
+
+            return [...products];
+        });
+    };
+
+    return(
+        <Box className={classes.favoritepageWrapper}>
+            <Typography component="span"
+                fontSize="30px"
+                fontWeight={600}
+                className={ classes.favoriteHeader}
+            >
+                <FavoriteIcon sx={{marginRight: '10px', color: "#FD6637"}}  />
+                Favorite
+            </Typography>
+
+             <Grid container spacing={2} mb={5} mx='auto'>
+				<For each={products} children={(product) => (
+					<Grid item xs={6} md={3} mb={3}>
+						<ProductCard product={product} onFavourite={onFavourite} to="/product/1" key={product.id} addToCart />
+					</Grid>
+				)} />
+			</Grid>
+        </Box>
+    )
 };
 
 export default FavouritePage;
--- frontend/src/_home/pages/Home.js
+++ frontend/src/_home/pages/Home.js
@@ -5,74 +5,18 @@ import BottomBanner from "../components/BottomBanner";
 import Footer from "../../common/components/Footer";
 import { useState } from "react";
 import SuggestionSection from "../components/SuggestionSection";
-
-const Bestseller1 = "https://hbr.org/resources/images/article_assets/2019/11/Nov19_14_sb10067951dd-001.jpg";
+import fakeProducts from "~/common/faker/fakeProducts";
 
 const HomePage = () => {
-  const itemsData = [
-    {
-      id: 0,
-      title: "Cheese Pizza very อร่อย มากๆๆๆ",
-      price: "500",
-      status: "Hot sale",
-      favourite: true,
-      image: Bestseller1,
-    },
-    {
-      id: 1,
-      title: "Cheese Pizza",
-      price: "500",
-      status: "Hot sale",
-      favourite: false,
-      image: Bestseller1,
-    },
-    {
-      id: 2,
-      title: "Cheese Pizza",
-      price: "500",
-      status: "Hot sale",
-      favourite: false,
-      image: Bestseller1,
-    },
-    {
-      id: 3,
-      title: "Cheese Pizza",
-      price: "500",
-      status: "Hot sale",
-      favourite: false,
-      image: Bestseller1,
-    },
-    {
-      id: 4,
-      title: "Cheese Pizza",
-      price: "500",
-      status: "Hot sale",
-      favourite: true,
-      image: Bestseller1,
-    },
-    {
-      id: 5,
-      title: "Cheese Pizza",
-      price: "500",
-      status: "Hot sale",
-      favourite: true,
-      image: Bestseller1,
-    },
-    {
-      id: 6,
-      title: "Cheese Pizza",
-      price: "500",
-      status: "Hot sale",
-      favourite: false,
-      image: Bestseller1,
-    },
-  ];
-  const [items, setItems] = useState(itemsData);
-  const onFavourite = (e, idx) => {
-    e.preventDefault();
-    setItems((items) => {
-      items[idx].favourite = !items[idx].favourite;
-      return [...items];
+  console.log(fakeProducts);
+  const [products, setProducts] = useState(fakeProducts);
+  
+  const onFavourite = (index) => {
+    setProducts((products) => {
+      const target = products[index];
+      target.favourite = !target.favourite;
+
+      return [...products];
     });
   };
 
@@ -80,8 +24,8 @@ const HomePage = () => {
     <>
       <TopBanner />
       <CategoriesSection />
-      <BestsellerSection bestsellerItems={items} onFavourite={onFavourite} />
-      <SuggestionSection suggestItems={itemsData} onFavourite={onFavourite} />
+      <BestsellerSection bestsellerItems={products} onFavourite={onFavourite} />
+      <SuggestionSection suggestItems={products} onFavourite={onFavourite} />
       <BottomBanner />
       <Footer />
     </>
