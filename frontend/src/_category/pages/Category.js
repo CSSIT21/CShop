3326655046f2f0b4db3b5326ea69e8be8532d821
suisuiss <74504881+suisuiss@users.noreply.github.com>
2021-10-26 21:52:24 +0700

    [ADD] search category filter (#21)

--- frontend/src/_category/pages/Category.js
+++ frontend/src/_category/pages/Category.js
@@ -1,7 +1,42 @@
-const CategoryPage = props => {
-    return <div>
-        Hello, this is CategoryPage
-    </div>;
+import React from 'react';
+
+import { Box, Divider } from '@mui/material';
+import { FilterAltOutlined } from '@mui/icons-material';
+import CategoryHeader from '../components/CategoryHeader';
+import { padding } from '@mui/system';
+import CategoryFilterPrice from '../components/CategoryFilterPrice';
+import CategoryFilterRate from '../components/CategoryFilterRate';
+import CateGoryFilterService from '../components/CategoryFilterService';
+import CategoryFilterAvailability from '../components/CategoryFilterAvailability';
+
+const CategoryPage = () => {
+  return (
+    <Box sx={{ padding: '25px 50px' }}>
+      <Box>
+        <CategoryHeader style={{ width: '100%' }} />
+      </Box>
+      <Box sx={{ display: 'flex' }}>
+        <Box sx={{ width: '20%' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: '50px 0px 20px',
+            }}
+          >
+            <b>Filter</b>
+            <FilterAltOutlined size='large' />
+          </Box>
+          <Divider />
+          <CategoryFilterPrice />
+          <CategoryFilterRate />
+          <CateGoryFilterService/>
+          <CategoryFilterAvailability/>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default CategoryPage;
