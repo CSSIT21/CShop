a13e2d80b13e9f214304afeb18131ff82839d608
Monthara Kiatruangchai <70026948+Axxmk@users.noreply.github.com>
2021-10-29 22:44:00 +0700

    [UPDATE] Navbar ratio (#36)

--- frontend/src/common/components/NavBarBase/ContentBase/AccountDropdown.jsx
+++ frontend/src/common/components/NavBarBase/ContentBase/AccountDropdown.jsx
@@ -6,29 +6,29 @@ import authState from '~/common/store/authState';
 import DropdownDetail from './DropdownDetail';
 
 const AccountDropdown = () => {
-  const classes = useStyles();
-  const auth = useRecoilValue(authState);
+	const classes = useStyles();
+	const { user } = useRecoilValue(authState);
 
-  return (
-    <Box className={classes.account}>
-      <DropdownDetail>
-        <span className={classes.accountName}>{auth.user.first_name} </span>
-        <Avatar src={auth.user.url} sx={{ width: 30, height: 30 }} />
-      </DropdownDetail>
-    </Box>
-  );
-};
+	return (
+		<Box className={classes.account}>
+			<DropdownDetail >
+				<span className={classes.accountName}>{user.first_name} </span>
+				<Avatar src={user.url} sx={{ width: 30, height: 30 }} />
+			</DropdownDetail>
+		</Box>
+	)
+}
 
 const useStyles = makeStyles({
-  account: {
-    display: "flex",
-    alignItems: "center",
-  },
-  accountName: {
-    width: "100%",
-    paddingRight: 10,
-    color: "black",
+	account: {
+		display: 'flex',
+		alignItems: 'center'
+	},
 
+	accountName: {
+		width: '100%',
+		paddingRight: 10,
+		color: 'black',
     textTransform: "capitalize",
     fontWeight: 500,
   },
