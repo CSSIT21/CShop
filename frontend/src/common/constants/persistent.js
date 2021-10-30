4d036134a17cba52f91130bc96fda3a1f39ddaa4
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-10-31 04:52:45 +0700

    [ADD] Custom hook "usePersistentState()"

---
+++ frontend/src/common/constants/persistent.js
@@ -0,0 +1,5 @@
+const persistent = {
+    
+};
+
+export default persistent;
--- frontend/src/common/hooks/index.js
+++ frontend/src/common/hooks/index.js
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useState, useLayoutEffect, useEffect } from 'react';
 import qs from "qs";
 import { useLocation } from "react-router";
+import SecureLS from 'secure-ls';
+import { isArray, isFunc, isNull, isObj } from '../utils';
+export let lsEffects = [];
+var persistStateIdx = 0;
 
-import { isArray, isFunc, isObj } from '../utils';
+export const ls = new SecureLS({
+    encodingType: 'rc4',
+    isCompression: false,
+    encryptionSecret: 's3cr3t$@1'
+});
 
 export const useStateFlush = (init=null,flush=false) => {
     const [state, setState] = useState(init);
@@ -45,5 +53,67 @@ export const useForm = (init={}) => {
 
 export function useQuery() {
     return qs.parse(useLocation().search, { ignoreQueryPrefix: true });
-  }
-  
+}
+
+export function useLsEffect(fn){
+    return useEffect(() => {
+        lsEffects.push(fn);
+        return () => lsEffects = [];
+    },[]);
+}
+
+export function emitLsEffect(){
+    return lsEffects.forEach(ev => ev());
+}
+
+export function usePersistState(init, key=null){
+    if(isNull(key)) key = `_persist_state_${persistStateIdx++}`;
+    let myInit = ls.get(key) ? JSON.parse(ls.get(key)) : (() => { ls.set(key,JSON.stringify(init)); return init})();
+    const [state, setState] = useState(myInit);
+    
+    useLayoutEffect(() => {
+        persistStateIdx = 0;
+    });
+
+    useLsEffect(() => {
+        var data = init;
+        try{
+            data = JSON.parse(ls.get(key))
+            setState(data);
+        }catch(e){
+            setState(init);
+        }
+    });
+    useEffect(() => {
+        ls.set(key, JSON.stringify(state));
+        const handler = ev => {
+            setState(JSON.parse(ls.get(key)));
+        };
+        window.addEventListener('storage', handler ,false);
+        return () => window.removeEventListener('storage', handler ,false);
+    }, [state]);
+
+    return [state,setState];
+}
+
+export const useLocalStorage = () => {
+    // const _ls = ls;
+    let myLs = Object.assign(Object.create(Object.getPrototypeOf(ls)), ls);
+    var props = ["clear",
+    "remove",
+    "removeAll",
+    "resetAllKeys",
+    "set",
+    "getEncryptionSecret",
+    "setMetaData",
+    "setDataToLocalStorage"];
+
+    for(let prop of props){
+        myLs[prop] = (...params) => {
+            ls[prop](...params);
+            emitLsEffect();
+            persistStateIdx = 0;
+        };
+    }
+    return myLs;
+}
