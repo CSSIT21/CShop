c84955cba6cb20657e9ccb43c3cd4ff4694b623b
maxky49221 <ratchanon.maxky@mail.kmutt.ac.th>
2021-11-11 21:45:45 +0700

    [UPDATE] promotion page

--- frontend/src/_promotion/components/Coupon.jsx
+++ frontend/src/_promotion/components/Coupon.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-function Coupon({namepro,detail,date}) {
+
+function Coupon({namepro,detail,date,picture}) {
+    const theme = useTheme();
+    
     return (
-        <div>
-                <h2>{namepro}</h2>
-                <h1>Detail</h1>
-                <p>{detail}</p>
-                <h1>Valid</h1>
-                <p>{date}</p>
-         
+        <div >
+       <Card sx={{ display: 'flex',marginBottom:'40px', marginLeft:'50vh',width: '700px' , alignItems:'center', height:'200px' , backgroundColor:'yellow' }} >
+      <Box sx={{ display: 'flex', flexDirection: 'column' ,backgroundColor: 'black'}}>
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            {namepro}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            Mac Miller
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+         Detail
+         {detail}
+         Invalid
+         {date}
+        </Box>
+      </Box>
+      <CardMedia
+        component="img"
+        sx={{ width: 151 }}
+        image="/static/images/cards/live-from-space.jpg"
+        alt="Live from space album cover"
+      />
+    </Card>
         </div>
     )
 }
--- frontend/src/_promotion/pages/Promotion.jsx
+++ frontend/src/_promotion/pages/Promotion.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import Coupon from '../components/Coupon';
-const Promotion = () => {
-    return (
-        <div>
-        <h3>Type_Shop</h3>
-        <Coupon/>            
-        </div>
-    )
-}
+import React, { Fragment } from "react";
+import Coupon from "../components/Coupon";
+import Nametype from "../components/Nametype";
+import { makeStyles } from "@mui/styles";
 
-export default Promotion
+const Promotion = () => {
+    const classes = useStyles();
+  return (
+    <>
+
+    <div class="main-content">
+        <div class="slide">
+            <figure>
+
+          <h1>image</h1>
+            
+            </figure>
+        </div>
+    </div>
+
+ 
+      <Nametype name="Shop" />
+      <Coupon
+        namepro="50% save for new user!!"
+        detail="This coupon can be use to have 50% discount"
+        date="31/12/2021"
+      />
+
+      <Coupon
+        namepro="50% save for new user!!"
+        detail="This coupon can be use to have 50% discount"
+        date="31/12/2021"
+      />
+
+      <Nametype name="App" />
+      <Coupon
+        namepro="50% save for new user!!"
+        detail="This coupon can be use to have 50% discount"
+        date="31/12/2021"
+      />
+
+      <Coupon
+        namepro="50% save for new user!!"
+        detail="This coupon can be use to have 50% discount"
+        date="31/12/2021"
+      />
+
+      <Nametype name="Event" />
+      <Coupon
+        namepro="50% save for new user!!"
+        detail="This coupon can be use to have 50% discount"
+        date="31/12/2021"
+      />
+    </>
+  );
+};
+
+const useStyles = makeStyles({
+  
+  });
+
+export default Promotion;
