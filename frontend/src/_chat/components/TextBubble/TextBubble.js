06ea2ed0f182ed5f1f43a2e4c046bce416b90b71
anakint <anakin.t@mail.kmutt.ac.th>
2021-11-13 00:30:08 +0700

    Add TextBubble component

--- frontend/src/_chat/components/TextBubble/TextBubble.js
+++ frontend/src/_chat/components/TextBubble/TextBubble.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Box } from '@mui/system';
 
 import useStyles from './TextBubble.styles'
+import { Typography } from '@mui/material';
 
 const TextBubble = props => {
     const classes = useStyles()
 
-    return <Box className={classes.container}>
-        Chat bubble for text
+    return <Box className={props.variant === 'right' ? classes.textBubbleRight : classes.textBubbleLeft}>
+        <Typography>{props.text}</Typography>
     </Box>;
 };
 
