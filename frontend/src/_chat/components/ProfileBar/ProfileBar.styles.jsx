ada13936d78f4c4853c9c703f14f2679e3325bdc
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2021-11-15 00:51:38 +0700

    Update ChatBox to handle overflow

--- frontend/src/_chat/components/ProfileBar/ProfileBar.styles.jsx
+++ frontend/src/_chat/components/ProfileBar/ProfileBar.styles.jsx
@@ -1,60 +1,62 @@
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
-    goToShopButton: {
-        color: 'white',
-        borderRadius: "7.14px !important"
-    },
-    profileBarContainer:{
-        display:'flex',
-        flexDirection:'row',
-        width: '100%',
-      height: '100%',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      
-      
-    },
-    avatar: {
-        width: '48px',
-        height: '48px',
-        
-      },
-      buttonZone:{
-        display:'flex',
-        flexDirection:'row',
-        alignItems:'flex-end',
-        
-        
-      },
-      textZone: {
-        display:'flex',
-        flexDirection:'row',
-        alignItems:'center',
-        marginLeft: '20px',
-       
-      },
-      profileBarDisplayName:{
-          display:'flex',
-          flexDirection:'column',
-          
-          marginLeft: '15px'
-      }
-      ,profileName:{ 
-          
-            fontSize: '22px !important',
-          
-      },
-      profileBarNoti:{
-        color:"#323232",
-        width:"15px !important",
-        height:"16px !important",
-        margin:"0px"
-      },
-      profileStatus:{
-        fontSize: '16px !important',
-        color: '#A0A3BD !important'
-      }
+  goToShopButton: {
+    color: "white",
+    borderRadius: "7.14px !important",
+  },
+  profileBarContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    height: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  avatar: {
+    width: "48px",
+    height: "48px",
+  },
+  buttonZone: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  textZone: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: "20px",
+    
+  },
+  profileBarDisplayName: {
+    display: "flex",
+    flexDirection: "column",
+
+    marginLeft: "15px",
+  },
+  profileName: {
+    
+    fontSize: "22px !important",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    width: "calc(50vw - 100px)",
+  },
+  profileBarNoti: {
+    color: "#323232",
+    width: "15px !important",
+    height: "16px !important",
+    margin: "0px",
+  },
+  profileStatus: {
+    fontSize: "16px !important",
+    color: "#A0A3BD !important",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    width: "calc(30vw - 100px)",
+  },
 });
 
 export default useStyles;
