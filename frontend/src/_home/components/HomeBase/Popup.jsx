2f4a7e506f10654c3fa35c165eb4843535ad6fe7
Ms.MONTHARA KIATRUANGCHAI <monthara.k@mail.kmutt.ac.th>
2021-11-15 19:29:45 +0700

    [UPDATE] add close button

--- frontend/src/_home/components/HomeBase/Popup.jsx
+++ frontend/src/_home/components/HomeBase/Popup.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
 import {
-	Dialog, 
-	DialogActions, 
-	DialogContent, 
-	DialogContentText, 
-	DialogTitle, 
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
 	Checkbox,
 	Box,
 	Paper,
 	Typography
 } from '@mui/material';
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
 import { makeStyles } from '@mui/styles';
 import { useRecoilValue } from 'recoil';
 import authState from '~/common/store/authState';
 
 const popup = {
+	picture: "https://cdn.discordapp.com/attachments/900689889086046218/909339280608989234/png-popup.png",
 	description: "Our websit will be closed for renovation. Sorry for inconvenience",
 	start_date: "2022/02/12",
 	end_date: "2022/02/14",
@@ -42,46 +45,48 @@ const Popup = () => {
 			aria-describedby="alert-dialog-description"
 			PaperComponent={Box}
 		>
+			<CancelRoundedIcon className={classes.closeStyle} sx={{ fontSize: "1.9rem" }} onClick={handleClose} />
+
 			<Paper className={classes.paperStyle}>
-				<img className={classes.imgStyle} width={75} src={"https://cdn.discordapp.com/attachments/900689889086046218/909339280608989234/png-popup.png"} />
-				
+				<img className={classes.imgStyle} width={75} src={popup.picture} />
+
 				<DialogTitle id="alert-dialog-title">
-					{isLoggedIn 
+					{isLoggedIn
 						? `Hi ${user.first_name}!`
 						: `Hi User!`
 					}
 				</DialogTitle>
 
 				<DialogContent>
-					<DialogContentText sx={{width: 300, textAlign: "center"}} id="alert-dialog-description">
+					<DialogContentText sx={{ width: 300, textAlign: "center" }} id="alert-dialog-description">
 						{popup.description}
 					</DialogContentText>
 
-					<Typography fontSize="12px"	fontWeight={400} color="#A0A3BD" mt={4} sx={{textAlign: "center"}}>
+					<Typography fontSize="12px" fontWeight={400} color="#A0A3BD" mt={4} sx={{ textAlign: "center" }}>
 						During {popup.start_date} - {popup.end_date}
 					</Typography>
 				</DialogContent>
 			</Paper>
 
-			<DialogActions sx={{display: "flex", justifyContent: "flex-start"}}>
-				<Checkbox 
-					checked={checked} 
-					onChange={onCheck} 
+			<DialogActions sx={{ display: "flex", justifyContent: "flex-start" }}>
+				<Checkbox
+					checked={checked}
+					onChange={onCheck}
 					size="small"
-					sx={{color: "#FD6637"}}
+					sx={{ color: "#FD6637" }}
 				/>
-				<Typography	fontSize="12px"	fontWeight={400} color="#CCCCCC" > 
+				<Typography fontSize="12px" fontWeight={400} color="#CCCCCC" >
 					Not show all day
 				</Typography>
 			</DialogActions>
-      	</Dialog>
+		</Dialog>
 	);
 };
 
 const useStyles = makeStyles({
 	paperStyle: {
-		display: "flex", 
-		flexDirection: "column", 
+		display: "flex",
+		flexDirection: "column",
 		alignItems: "center",
 	},
 
@@ -89,6 +94,18 @@ const useStyles = makeStyles({
 		marginBottom: "10px",
 		display: "block",
 		transform: "translate(0, -70px) scale(4)",
+	},
+
+	closeStyle: {
+		color: "white",
+		position: "absolute",
+		top: "-17%",
+		right: "-8%",
+		cursor: "pointer",
+
+		"&:hover": {
+			color: "#FD6637",
+		},
 	},
 });
 
