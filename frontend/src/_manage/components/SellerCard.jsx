aa43ed5e45b260ff62e71a3c745e1dd28394afa3
iceing11 <ice_iceing@hotmail.com>
2021-11-17 02:56:41 +0700

    [ADD] Seller and seller card

---
+++ frontend/src/_manage/components/SellerCard.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import { Card } from "@mui/material";
+import { CardContent } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import { Collapse } from "@mui/material";
+import { IconButton } from "@mui/material";
+import { Typography } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { CardLayout } from "./UserCardStyled";
+import { Avatar } from "@mui/material";
+import { grey, lightBlue, amber } from '@mui/material/colors';
+import { withStyles } from "@mui/styles";
+import { Box } from "@mui/system";
+import { Button } from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+export class SellerCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expand: false
+    };
+  }
+
+  handleExpandClick = () => {
+    this.setState({ expand: !this.state.expand });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <CardLayout elevation={0}>
+        <CardContent>
+          <Box className={classes.header}>
+            <Box sx={{ width: '7%' }} className={classes.header}>
+              <Avatar sx={{ bgcolor: this.props.seller.avatarColor, width: 60, height: 60 }}>{this.props.seller.avatarInitials}</Avatar>
+            </Box>
+            <Box sx={{ width: '17%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+              <Typography noWrap style={{ fontSize: '15px'}}>#{this.props.seller.id}</Typography>
+            </Box>
+            <Box sx={{ width: '20%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+            <Typography noWrap style={{ fontWeight: 600, fontSize: '15px'}}>{this.props.seller.name}</Typography>
+            </Box>
+            <Box sx={{ width: '8%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+              <Typography style={{ fontSize: '15px', textAlign: 'center'}}>{this.props.seller.productCount}</Typography>
+            </Box>
+            <Box sx={{ width: '9.5%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+              <Typography style={{ fontSize: '15px', textAlign: 'center'}}>{this.props.seller.followers}</Typography>
+            </Box>
+            <Box sx={{ width: '13%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+              <Typography style={{ fontSize: '15px', textAlign: 'center'}}>{this.props.seller.cancelRate}</Typography>
+            </Box>
+            <Box sx={{ width: '13%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+              <Typography style={{ fontSize: '15px', textAlign: 'center'}}>{this.props.seller.joinDate}</Typography>
+            </Box>
+            <Box sx={{ width: '10%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}> 
+              <div style={{ display:'flex', justifyContent:'center' }}>
+              { this.props.seller.rating < 2.5 ?
+                <Card variant="outlined" style={{
+                  backgroundColor: "#E04A4A33",
+                  border: 'none',
+                  width: '75%'}}>
+                <Typography style={{ fontSize: '15px', textAlign: 'center', color: '#812525'}}>{this.props.seller.rating}</Typography>
+                </Card> :
+                (
+                  this.props.seller.rating > 3.5 ?
+                  <Card variant="outlined" style={{
+                    backgroundColor: "#B3E24B33",
+                    border: 'none',
+                    width: '75%'}}>
+                  <Typography style={{ fontSize: '15px', textAlign: 'center', color: '#5B8125'}}>{this.props.seller.rating}</Typography>
+                  </Card> :
+                  <Card variant="outlined" style={{
+                    backgroundColor: "#F4AF5433",
+                    border: 'none',
+                    width: '75%'}}>
+                  <Typography style={{ fontSize: '15px', textAlign: 'center', color: '#D28C40'}}>{this.props.seller.rating}</Typography>
+                  </Card>
+                )
+              }
+              </div>
+            </Box>
+            <Box sx={{ width: '2%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+            {this.state.expand === true ? 
+              <KeyboardArrowUpIcon sx={{ marginLeft: '3px', fontSize: 25}} color="primary" onClick={this.handleExpandClick}/> : 
+              <KeyboardArrowDownIcon sx={{ marginLeft: '3px', fontSize: 25}} color="primary" onClick={this.handleExpandClick}/> }
+            </Box>
+          </Box>
+        </CardContent>
+        <Collapse in={this.state.expand} timeout="auto" unmountOnExit>
+          <CardContent className={classes.header} sx={{ padding:'0px' }}>
+            <Typography noWrap style={{ fontWeight: 600, fontSize: '15px', margin:'20px'}}>Restrictions ({this.props.seller.restrictions.length})</Typography>
+            <Button variant="contained" sx={{ height:'50%', margin:'12px' }}>Add</Button>
+          </CardContent>
+          <CardContent>
+          {this.props.seller.restrictions.map((res) => (
+                            <div key={res.id.toString()}>
+                                <Card variant="outlined" style={{
+                                backgroundColor: "#FFEDE7",
+                                border: 'none',
+                                marginBottom: '15px'}}>
+                                  <CardContent className={classes.header} sx={{ padding:'28px' }}>
+                                    <Box sx={{ width: '22%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+                                      <Typography noWrap style={{ fontSize: '15px', color: '#FD6637'}}>Restriction Type</Typography>
+                                      <Typography noWrap style={{ fontWeight: 600, fontSize: '15px'}}>{res.type}</Typography>
+                                    </Box>
+                                    <Box sx={{ width: '15%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+                                      <Typography noWrap style={{ fontSize: '15px', color: '#FD6637'}}>Assigner</Typography>
+                                      <Typography noWrap style={{ fontWeight: 600, fontSize: '15px'}}>{res.assigner}</Typography>
+                                    </Box>
+                                    <Box sx={{ width: '12%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+                                      <Typography noWrap style={{ fontSize: '15px', color: '#FD6637'}}>Start Time</Typography>
+                                      <Typography noWrap style={{ fontWeight: 600, fontSize: '15px'}}>{res.startTime}</Typography>
+                                    </Box>
+                                    <Box sx={{ width: '12%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+                                      <Typography noWrap style={{ fontSize: '15px', color: '#FD6637'}}>End Time</Typography>
+                                      <Typography noWrap style={{ fontWeight: 600, fontSize: '15px'}}>{res.endTime}</Typography>
+                                    </Box>
+                                    <Box sx={{ width: '30%', display:'flex', flexDirection: 'column', justifyContent: 'center' }}>  
+                                      <Typography style={{ fontSize: '15px', color: '#FD6637'}}>Description</Typography>
+                                      <Typography style={{ fontWeight: 600, fontSize: '15px'}}>{res.desc}</Typography>
+                                    </Box>
+                                    <Box sx={{ width: '10%', display:'flex', flexDirection: 'column', justifyContent: 'center'}}>  
+                                      <Button variant="contained" sx={{ width: '50%', marginLeft: '35px'}}><DeleteIcon/></Button>
+                                    </Box>
+                                  </CardContent>
+                                </Card>
+                            </div>
+                        ))}
+          </CardContent>
+          <CardContent className={classes.header} sx={{ padding:'0px' }}>
+            <Button variant="outlined" sx={{ marginLeft:'30px' }}>Go to Transaction History</Button>
+          </CardContent>
+        </Collapse>
+      </CardLayout>
+    );
+  }
+}
+
+const styles = theme => ({
+  header: {
+    display:'flex',
+    flexDirection: 'row',
+  }
+});
+
+export default withStyles(styles, { withTheme: true })(SellerCard);
--- frontend/src/_manage/pages/Sellers.jsx
+++ frontend/src/_manage/pages/Sellers.jsx
@@ -1,17 +1,270 @@
+import Paper from '@mui/material/Paper';
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { List } from "@mui/material";
+import { For } from "~/common/utils/index";
+import { styled } from '@mui/material/styles';
+import { Pagination } from '@mui/material';
+import { Card } from '@mui/material';
+import { CardContent } from '@mui/material';
+import { Grid } from '@mui/material';
+import { FormGroup } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import { Checkbox } from '@mui/material';
+import { makeStyles } from "@mui/styles";
+import { TextField } from '@mui/material';
+import { FormControl } from '@mui/material'
+import { InputLabel } from '@mui/material';
+import { Select } from '@mui/material';
+import { MenuItem } from '@mui/material';
+import { FormHelperText } from '@mui/material';
+import { InputAdornment } from '@mui/material';
+import { createTheme } from '@mui/material';
 import CButton from "../../common/components/CButton";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import React, { Fragment, useEffect, useState, useLayoutEffect } from "react";
+import SellerCard from "../components/SellerCard";
+import { Search } from '@mui/icons-material';
+import { grey, red, amber, orange, pink, deepPurple, blue, lightGreen, brown } from '@mui/material/colors';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+const cardStyle = {
+    width: '100%',
+    padding: '0px',
+    margin: '0px',
+  
+    border: 'none',
+    transition: "all ease 0.125s",
+  };
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+let sellers = [
+    {
+        id: 2000,
+        avatarInitials: 'LB',
+        avatarColor: brown[600],
+        name: 'The Library',
+        productCount: 46,
+        followers: 254685,
+        joinDate: '15/05/2020',
+        cancelRate: '2%',
+        rating: 3.2,
+        restrictions: [
+            {id:2, type:'Nest Relocation', assigner:'CuteArbiterZena', startTime:'08/10/2020', endTime:'01/12/2021', desc:'Violating the AI Ethics Amendment'}
+        ]
+    },
+    {
+        id: 4444,
+        avatarInitials: '死',
+        avatarColor: red[800],
+        name: 'Shi Association',
+        productCount: 44,
+        followers: 44444,
+        joinDate: '04/04/2014',
+        cancelRate: '4%',
+        rating: 4.4,
+        restrictions: []
+    }
+    ,
+    {
+        id: 1201,
+        avatarInitials: 'LC',
+        avatarColor: red[400],
+        name: 'Lobotomy Corporation',
+        productCount: 128,
+        followers: 123463,
+        joinDate: '12/07/2016',
+        cancelRate: '21%',
+        rating: 1.3,
+        restrictions: [
+            {id:24, type:'Transaction Restrictions', assigner:'AdminGarion', startTime:'22/11/2016', endTime:'22/12/2016', desc:'Improper Handling of Products'},
+            {id:25, type:'Full Restrictions', assigner:'AdminGarion', startTime:'22/12/2016', endTime:'31/12/2999', desc:'Improper Handling of Products'}
+        ]
+    }
+];
 
 const ManageSellerAccountPage = () => {
+    const classes = useStyles();
+    const [sortBy, setSortBy] = React.useState('');
+    const setSort = (event) => {
+        setSortBy(event.target.value);
+      };
+
+    const [sortOrder, setSortOrder] = React.useState(false);
+    const toggleSort = () => {
+        setSortOrder(!sortOrder);
+      }
+
+    const [showRestricted, setShowRestricted] = React.useState(false);
+    const toggleShowRestricted = () => {
+        setShowRestricted(!showRestricted);
+        setPage(1);
+        }
+
+    const [page, setPage] = React.useState(1);
+    const handlePagination = (event) => {
+        setPage(event.target.textContent);
+    }
+
+    const [search, setSearch] = React.useState('');
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    }
+
+    useLayoutEffect(() => {
+        document.body.classList.add("gray");
+        document.body.classList.add("no-scroll!important")
+        return () => {document.body.classList.remove("gray")};
+      }, []);
+
     return (
-        <Box>
-            <Typography variant="h4" fontWeight="600">
-                Manage Sellers
-            </Typography>
-            <CButton icon={<AccountBalanceIcon />} title="Mixko" />
-        </Box>
+        <div>
+            <Box className={classes.topwrapper}>
+                <Box className={classes.topright}>
+                    <FormGroup>
+                        <FormControlLabel onChange={toggleShowRestricted} control={<Checkbox />} label="Show Restricted Only" />
+                    </FormGroup>
+                </Box>
+                <Box className={classes.topright}>
+                    <Box sx={{ margin: '8px'}}>
+                        <TextField
+                        hiddenLabel
+                        id="search-field"
+                        placeholder="Search"
+                        variant="filled"
+                        size="small"
+                        onChange={handleSearch}
+                        className={classes.root2}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                <Search color="primary"/>
+                                </InputAdornment>
+                                ),
+                                className: classes.input,
+                            }}
+                        />
+                    </Box>
+                    <Box sx={{ marginTop: '10px', marginBottom: '10px', display:'flex', justifyContent: 'flex-end'}}>
+                        <FormGroup sx={{ margin: '6px'}}>
+                            <FormControlLabel onChange={toggleSort} control={<Checkbox />} label="Sort by Ascending"/>
+                        </FormGroup>
+                        <FormControl sx={{ m: 1 }}>
+                            <InputLabel id="sort-by-select-label" sx={{ top: '-5px' }}>Sort By</InputLabel>
+                            <Select
+                              labelId="sort-by-label"
+                              id="sort-by"
+                              value={sortBy}
+                              label="Sort By"
+                              className={classes.root}
+                              onChange={setSort}
+                            >
+                              <MenuItem value={'name'}>Name</MenuItem>
+                              <MenuItem value={'id'}>User ID</MenuItem>
+                              <MenuItem value={'productCount'}>Products</MenuItem>
+                              <MenuItem value={'followers'}>Followers</MenuItem>
+                              <MenuItem value={'cancelRate'}>Cancel Rate</MenuItem>
+                              <MenuItem value={'joinDate'}>Join Date</MenuItem>
+                              <MenuItem value={'rating'}>Rating</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Box>
+            </Box>
+            <Card variant="outlined" sx={cardStyle}>
+                <Card variant="outlined" style={{
+                    backgroundColor: "#FDF4DD",
+                    border: 'none',
+                    margin: '25px',
+                    marginBottom: '0px'}}>
+                    <CardContent sx={{ padding: '15px', paddingBottom: '15px!important'}}>
+                    <Box className={classes.header}>
+                        <Box sx={{ width: '25%' }} className={classes.header}>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Sellers ({(showRestricted ? (sellers.filter(function( obj ) {return obj.status == 'Restricted';})).length : sellers.length)})</Typography>
+                        </Box>
+                        <Box sx={{ width: '20%' }} className={classes.header}>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Shop Name</Typography>
+                        </Box>
+                        <Box sx={{ width: '10%' }} className={classes.header}>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Products</Typography>
+                        </Box>
+                        <Box sx={{ width: '10%' }} className={classes.header}>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Followers</Typography>
+                        </Box>
+                        <Box sx={{ width: '14%' }} className={classes.header}>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Cancelled Rate</Typography>
+                        </Box>
+                        <Box sx={{ width: '12%' }} className={classes.header}>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Joined Date</Typography>
+                        </Box>
+                        <Box sx={{ width: '10%' }} className={classes.header}>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Rating</Typography>
+                        </Box>
+                    </Box>
+                    </CardContent>
+                </Card>
+            <List>
+                <Card variant="outlined" sx={cardStyle}>
+                    <CardContent>
+                        {
+                        (showRestricted ? 
+                            (sellers.filter(seller => seller.name.toUpperCase().includes(search.toUpperCase())).sort((a,b) => { return (a[sortBy] > b[sortBy]) ? (sortOrder ? -1 : 1) : (sortOrder ? 1 : -1) ; }))
+                            .filter(function( obj ) {
+                                return obj.restrictions.length > 0;
+                            })
+                            : (sellers.filter(seller => seller.name.toUpperCase().includes(search.toUpperCase())).sort((a,b) => { return (a[sortBy] > b[sortBy]) ? (sortOrder ? -1 : 1) : (sortOrder ? 1 : -1) ; }))
+                        ).slice((page -1)  * 10, (page - 1) * 10 + 10)
+                        .map((key) => (
+                            <li key={key.id.toString()}>
+                                <div style={{ display:'flex', justifyContent:'center' }}>
+                                    <SellerCard seller={key}/>
+                                </div>
+                            </li>
+                        ))}
+                    </CardContent>
+                </Card>
+            </List>
+            </Card>
+            <CardContent>
+                <div style={{ display:'flex', justifyContent:'center' }}>
+                    <Pagination count={Math.ceil(((showRestricted ? (sellers.filter(user => user.name.toUpperCase().includes(search.toUpperCase())).filter(function( obj ) {return obj.status == 'Restricted';})).length : sellers.filter(user => user.name.toUpperCase().includes(search.toUpperCase())).length - 1))/10)} showFirstButton showLastButton color="primary" shape="rounded" onChange={handlePagination}/>
+                </div>
+            </CardContent>
+        </div>
     );
 };
+
+const useStyles = makeStyles({
+    topwrapper: {
+        display:'flex',
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "5px",
+    },
+    topright: {
+        display:'inline-block',
+        justifyContent: "right",
+        marginLeft: "15px",
+    },
+    root: {
+        width: "200px",
+        height: "40px"
+    },
+    root2: {
+        width: "450px"
+    },
+    input: {
+        color: 'white'
+    },
+    header: {
+        display:'flex',
+        flexDirection: 'row',
+    }
+});
 
 export default ManageSellerAccountPage;
