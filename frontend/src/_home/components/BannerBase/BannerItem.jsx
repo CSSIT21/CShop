edebc1d93d0087fd73dc4e35f65eb8baeb16c25e
Ms.MONTHARA KIATRUANGCHAI <monthara.k@mail.kmutt.ac.th>
2021-11-18 00:07:34 +0700

    [UPDATE] manage banner page

--- frontend/src/_home/components/BannerBase/BannerItem.jsx
+++ frontend/src/_home/components/BannerBase/BannerItem.jsx
@@ -1,35 +1,54 @@
+import { useRef, useLayoutEffect } from 'react';
 import { makeStyles } from "@mui/styles";
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { noop } from '~/common/utils';
+
+const BannerItem = ({
+	index = 0,
+	items = [],
+	setItems = noop,
+	item = null,
+	onNext = noop,
+	onPrev = noop
+}) => {
+	const wrapper = useRef(null);
+	const classes = useStyles();
+
+	useLayoutEffect(() => {
+		setItems(items => {
+			items[index].height = wrapper.current.offsetHeight;
+			return [...items];
+		})
+	}, [index]);
+
+	return (
+		<Box className={classes.bannerComponent} ref={wrapper}>
+			<Stack direction="column" gap={10}>
+				{index > 0 && <Box><Button sx={{ width: '100%' }} aria-label="prev" onClick={() => onPrev(index)}>
+					<ArrowDropUpIcon />
+				</Button></Box>}
+
+				<Box>{item.name} {item.name.split('').map((k, key) => <br key={key} />)}</Box>
+
+				{index < items.length - 1 && <Box><Button sx={{ width: '100%' }} aria-label="next" onClick={() => onNext(index)}>
+					<ArrowDropDownIcon />
+				</Button></Box>}
+			</Stack>
+		</Box>
+	);
+};
 
 const useStyles = makeStyles({
 	bannerComponent: {
-		width: "300px",
-		height: "100px",
+		width: "100%",
+		height: "auto",
 		backgroundImage: "linear-gradient(135deg, rgb(168, 237, 234) 0%, rgb(254, 214, 227) 100%)",
+		borderBottom: '2px solid #5353534c'
 	},
 });
-
-const BannerItem = ({ banner }) => {
-
-	const classes = useStyles();
-
-	return (
-		<>
-			<IconButton aria-label="prev">
-				<ArrowDropUpIcon />
-			</IconButton>
-
-			<Box className={classes.bannerComponent}>
-
-			</Box>
-
-			<IconButton aria-label="next">
-				<ArrowDropDownIcon />
-			</IconButton>
-		</>
-	);
-};
 
 export default BannerItem;
--- frontend/src/_home/pages/ManageBanner.jsx
+++ frontend/src/_home/pages/ManageBanner.jsx
@@ -1,84 +1,135 @@
 import { useState, useEffect } from 'react';
 import { useTransition, animated } from 'react-spring';
+import { makeStyles } from "@mui/styles";
 import BannerItem from '../components/BannerBase/BannerItem';
+import Box from '@mui/material/Box';
 
 const data = [
 	{
 		name: 'Rare Wind',
 		description: '#a8edea → #fed6e3',
-		css: 'linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)',
-		height: 150
+		height: 500
 	},
 	{
 		name: 'Saint Petersburg',
 		description: '#f5f7fa → #c3cfe2',
-		css: 'linear-gradient(135deg, #c3cfe2 0%, #c3cfe2 100%)',
-		height: 150
+		height: 500
 	},
 	{
 		name: 'Deep Blue',
 		description: '#e0c3fc → #8ec5fc',
-		css: 'linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)',
-		height: 200
+		height: 500
 	},
 	{
 		name: 'Ripe Malinka',
 		description: '#f093fb → #f5576c',
-		css: 'linear-gradient(135deg, #f093fb 0%, #f5576c 100%)',
-		height: 140
+		height: 500
 	},
 	{
 		name: 'Near Moon',
 		description: '#5ee7df → #b490ca',
-		css: 'linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)',
-		height: 200
+		height: 500
 	},
 	{
 		name: 'Wild Apple',
 		description: '#d299c2 → #fef9d7',
-		css: 'linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)',
-		height: 150
+		height: 500
 	},
 	{
 		name: 'Ladoga Bottom',
 		description: '#ebc0fd → #d9ded8',
-		css: 'linear-gradient(135deg, #ebc0fd 0%, #d9ded8 100%)',
-		height: 160
+		height: 500
 	},
 	{
 		name: 'Sunny Morning',
 		description: '#f6d365 → #fda085',
-		css: 'linear-gradient(120deg, #f6d365 0%, #fda085 100%)',
-		height: 140
+		height: 500
 	},
 	{
 		name: 'Lemon Gate',
 		description: '#96fbc4 → #f9f586',
-		css: 'linear-gradient(to top, #96fbc4 0%, #f9f586 100%)',
-		height: 200
+		height: 500
 	}
 ];
 
+
 const ManageBanner = () => {
+	const classes = useStyles();
 	const [items, setItems] = useState(data);
 
 	let height = 0
 	const transitions = useTransition(
 		items.map(data => ({ ...data, y: (height += data.height) - data.height })),
-		d => { console.log(d); return d.name },
+		d => d.name,
 		{
 			from: { height: 0, opacity: 0 },
 			leave: { height: 0, opacity: 0 },
 			enter: ({ y, height }) => ({ y, height, opacity: 1 }),
 			update: ({ y, height }) => ({ y, height })
 		}
-	)
+	);
+
+	const onPrev = (index) => {
+		setItems((items) => {
+			return items.map((item, i) => {
+				if (i == index) {
+					return items[i - 1];
+				}
+				else if (i == index - 1) {
+					return items[i + 1];
+				}
+				return item;
+			})
+		})
+	};
+
+	const onNext = (index) => {
+		setItems((items) => {
+			return items.map((item, i) => {
+				if (i == index) {
+					return items[i + 1];
+				}
+				else if (i == index + 1) {
+					return items[i - 1];
+				}
+				return item;
+			})
+		})
+	};
 
 	return (
-		<>
-			<BannerItem />
-		</>
+		<Box style={{ height, position: "relative" }}>
+			{transitions.map(({ item, props: { y, ...rest }, key }, index) => (
+				<animated.div
+					key={key}
+					style={{
+						position: "absolute",
+						willChange: "transform, height, opacity",
+						width: "100%", zIndex: items.length - index, transform: y.interpolate(y => `translate3d(0,${y}px,0)`), ...rest
+					}}>
+					<Box className={classes.bannerWrapper}>
+						<BannerItem
+							items={items}
+							item={item}
+							index={index}
+							setItems={setItems}
+							onNext={onNext}
+							onPrev={onPrev}
+						/>
+					</Box>
+				</animated.div>
+			))}
+		</Box>
 	);
 };
 
+const useStyles = makeStyles({
+	bannerWrapper: {
+		position: "relative",
+		backgroundSize: "cover",
+		width: "100%",
+		height: "100%",
+		padding: "30px"
+	}
+});
 export default ManageBanner;
