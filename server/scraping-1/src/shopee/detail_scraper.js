de4eef2f7592ee591c182a69375f31faefc965a2
Thun (Mac Mini) <bsthun@gmail.com>
2021-11-18 14:49:45 +0700

    Scraping tuning #3

--- server/scraping-1/src/shopee/detail_scraper.js
+++ server/scraping-1/src/shopee/detail_scraper.js
@@ -17,7 +17,7 @@ const detail = async (browser, url) => {
 		// Images extraction
 		const images = [];
 		
-		await page.waitForSelector('._3-_YTZ', { timeout: 500 });
+		await page.waitForSelector('._3-_YTZ', { timeout: 30000 });
 		const imgThumbnails = await page.$$('._3-_YTZ');
 		await imgThumbnails[0].click();
 		await imgThumbnails[0].click();
@@ -37,26 +37,26 @@ const detail = async (browser, url) => {
 		}
 		
 		// Title extraction
-		await page.waitForSelector('.attM6y', { timeout: 500 });
+		await page.waitForSelector('.attM6y', { timeout: 1000 });
 		const title = await page.$$eval('.attM6y', (elems) => {
 			return elems[0].querySelector('span').textContent;
 		});
 		
 		// Price extraction
-		await page.waitForSelector('.Ybrg9j', { timeout: 500 });
+		await page.waitForSelector('.Ybrg9j', { timeout: 1000 });
 		const [price, priceRange] = await page.$$eval('.Ybrg9j', (elems) => {
 			const text = elems[0].textContent;
 			return [parseInt(text.split(' ')[0].substring(1).replace(/,/g, '')), text]; // Split base price in case of price be a range.
 		});
 		
 		// Description extraction
-		await page.waitForSelector('._3yZnxJ', { timeout: 500 });
+		await page.waitForSelector('._3yZnxJ', { timeout: 1000 });
 		const description = await page.$$eval('._3yZnxJ', (elems) => {
 			return elems[0].querySelector('span').textContent;
 		});
 		
 		// Quantity extraction
-		await page.waitForSelector('._90fTvx', { timeout: 500 });
+		await page.waitForSelector('._90fTvx', { timeout: 1000 });
 		const quantity = await page.$$eval('._90fTvx', (elems) => {
 			const text = elems[0].querySelector('.flex > div:last-child').textContent;
 			const matches = text.match(/\d+/g);
@@ -64,13 +64,13 @@ const detail = async (browser, url) => {
 		});
 		
 		// Sold extraction
-		await page.waitForSelector('.aca9MM', { timeout: 500 });
+		await page.waitForSelector('.aca9MM', { timeout: 1000 });
 		const sold = await page.$$eval('.aca9MM', (elems) => {
 			return parseInt(elems[0].textContent);
 		});
 		
 		// Rating extraction
-		await page.waitForSelector('.OitLRu', { timeout: 500 });
+		await page.waitForSelector('.OitLRu', { timeout: 1000 });
 		const [ratingValue, ratingCount] = await page.$$eval('.OitLRu', (elems) => {
 			return [parseInt(elems[0]?.textContent) || 0, parseInt(elems[1]?.textContent || 0)];
 		});
@@ -85,7 +85,7 @@ const detail = async (browser, url) => {
 		});
 		
 		// Subcategories extraction
-		await page.waitForSelector('._3wdEZ5 ._3QRNmL', { timeout: 500 });
+		await page.waitForSelector('._3wdEZ5 ._3QRNmL', { timeout: 1000 });
 		const subcategories = await page.$$eval('._3wdEZ5 ._3QRNmL:not(:first-child)', (elems) => {
 			return elems.map((el) => {
 				return el.textContent;
@@ -95,7 +95,7 @@ const detail = async (browser, url) => {
 		// Reviews extraction
 		const reviews = [];
 		
-		await page.waitForSelector('.product-ratings', { timeout: 500 });
+		await page.waitForSelector('.product-ratings', { timeout: 1000 });
 		const reviewNoData = await page.$$('.product-ratings-comments-view__no-data');
 		if (reviewNoData.length === 0) {
 			reviews.push(
