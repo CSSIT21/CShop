c29c9e581e08ae341ac0ca32e6fe81503e67f06b
2D <nonthakorn.inthong@mail.kmutt.ac.th>
2021-11-22 00:26:47 +0700

    [ADD] DiscountBody

--- frontend/src/_seller/pages/SellerDiscountLog.jsx
+++ frontend/src/_seller/pages/SellerDiscountLog.jsx
@@ -1,15 +1,166 @@
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
 import React from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import SellerSearch from "./components/SellerSearch";
+import authState from "../../common/store/authState";
+import { useRecoilValue } from "recoil";
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import LogHeader from "./components/LogHeader";
+import LogBody from "./components/LogBody";
+import LogFooter from "./components/LogFooter";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import HistoryDiscount from "./components/HistoryDiscountBody";
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+function createData(discountID, productName, customerName, used_date) {
+  return { discountID, productName, customerName, used_date };
+}
 
 const SellerDiscountLog = () => {
-    return (
-        <Box>
-            <Typography variant="h4" fontWeight="600">
-                Products SellerDiscountLog
-            </Typography>
-        </Box>
-    );
+  const Pagename = "Discount History";
+  const auth = useRecoilValue(authState);
+
+  const headerName = ["discountID", "productName", "customerName", "used_date"];
+
+  const rows = [
+    createData(1, "Chicken Wing", "John A", "2021-11-21 11:15:16"),
+    createData(2, "Chicken Wang", "John B", "2021-11-22 12:15:16"),
+    createData(3, "Chicken Wong", "John C", "2021-11-23 13:15:16"),
+    createData(4, "Chicken Weng", "John D", "2021-11-24 14:15:16"),
+    createData(5, "Chicken Wung", "John E", "2021-11-25 15:15:16"),
+    createData(6, "Chicken Bee", "John F", "2021-11-26 16:15:16"),
+    createData(7, "Chicken Aaa", "John G", "2021-11-27 17:15:16"),
+  ].sort((a, b) => (a.id < b.id ? -1 : 1));
+  // ------------------------------------
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Box>
+      <Box sx={{ mt: "4rem" }} />
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+          mb: 2,
+        }}
+      >
+        <SellerSearch Pagename={Pagename} />
+      </Box>
+      <Box sx={{ mt: "4rem" }} />
+
+      {/* <LogTable rows={rows} headerName={headerName} /> */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+          <LogHeader headerName={headerName} />
+          <HistoryDiscount
+            rowsPerPage={rowsPerPage}
+            page={page}
+            emptyRows={emptyRows}
+            rows={rows}
+          />
+          <LogFooter
+            rows={rows}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            handleChangePage={handleChangePage}
+            handleChangeRowsPerPage={handleChangeRowsPerPage}
+            TablePaginationActions={TablePaginationActions}
+          />
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 };
 
 export default SellerDiscountLog;
