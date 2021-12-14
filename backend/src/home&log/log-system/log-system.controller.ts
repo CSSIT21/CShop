7f39b4fe27f5dd4894820dc14dcee2eb1acd1a01
Aerith <chutirat.earth76@mail.kmutt.ac.th>
2021-12-14 21:06:21 +0700

    [ADD] discount-log backend

--- backend/src/home&log/log-system/log-system.controller.ts
+++ backend/src/home&log/log-system/log-system.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { LogSystemService } from './log-system.service';
 import { CreateAddToCardLogDto } from './dto/create-add-to-card-log.dto';
+import { CreateDiscountLogDto } from './dto/create-discount-log.dto';
 
 @Controller('log-system')
 export class LogSystemController {
@@ -12,10 +13,20 @@ export class LogSystemController {
       return this.logSystemService.createAddToCard(addTocardDto,customer_id,product_id)
     }
     catch (err){ 
-      this.logSystemService.throwError(err);
+      this.logSystemService.throwError(err); 
     }
     
   }  
+  @Post("discount/:customer_id/:discount_id")
+  async createDiscount(@Param('customer_id',ParseIntPipe) customer_id: number, @Param('discount_id',ParseIntPipe) discount_id: number, @Body() discountDto:CreateDiscountLogDto ) {
+    try {
+      return this.logSystemService.createDiscount(discountDto, customer_id, discount_id,)
+    }
+    catch (err) { 
+       this.logSystemService.throwError(err); 
+    }
+  } 
 
+ 
   
 }
