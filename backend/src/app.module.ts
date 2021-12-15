eef7f11e775433083218f95dde50f3c557009147
Mixko <apisit.mixko@mail.kmutt.ac.th>
2021-12-16 01:48:59 +0700

    [UPDATE] Database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/auth.module';
 import { JwtAuthGuard } from './common/guards/jwtAuth.guard';
 import { PrismaModule } from './prisma/prisma.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-	imports: [HttpModule, AuthenticationModule, PrismaModule],
+	imports: [HttpModule, AuthenticationModule, PrismaModule, DatabaseModule],
 	controllers: [AppController],
 	providers: [
 		// AppService,
--- backend/src/home&log/home/home.service.ts
+++ backend/src/home&log/home/home.service.ts
@@ -4,23 +4,23 @@ import { UpdateHomeDto } from './dto/update-home.dto';
 
 @Injectable()
 export class HomeService {
-  create(createHomeDto: CreateHomeDto) {
-    return 'This action adds a new home';
-  }
+	create(createHomeDto: CreateHomeDto) {
+		return 'This action adds a new home';
+	}
 
-  findAll() {
-    return `This action returns all home`;
-  }
+	findAll() {
+		return `This action returns all home`;
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} home`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} home`;
+	}
 
-  update(id: number, updateHomeDto: UpdateHomeDto) {
-    return `This action updates a #${id} home`;
-  }
+	update(id: number, updateHomeDto: UpdateHomeDto) {
+		return `This action updates a #${id} home`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} home`;
-  }
+	remove(id: number) {
+		return `This action removes a #${id} home`;
+	}
 }
