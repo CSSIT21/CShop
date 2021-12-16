12f6c95abc206a701851158419e4dc56a7f2bb32
Pathompong010 <70483000+Pepo-Pathompong@users.noreply.github.com>
2021-12-16 21:11:04 +0700

    [ADD] StockLog

--- backend/src/sellerconsole/sellerconsole.controller.ts
+++ backend/src/sellerconsole/sellerconsole.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Res } from '@nestjs/common';
 import { SellerconsoleService } from './sellerconsole.service';
 import { CreateSellerconsoleDto } from './dto/create-sellerconsole.dto';
 import { UpdateSellerconsoleDto } from './dto/update-sellerconsole.dto';
@@ -14,12 +14,39 @@ export class SellerconsoleController {
 		return this.sellerconsoleService.seller_dashboard(+id);
 	}
 
-	@Get('history')
+	@Get(':id/discountHistory')
     @Public()
-    getHistoryDiscount(){
+     async getHistoryDiscount(@Param('id') id : string){
         return this.sellerconsoleService.getHistoryDiscount();
     }
 
+	@Get(':id/stock')
+	@Public()
+	 getStock(@Param('id') id : number){
+		 return this.sellerconsoleService.getStock(+id);
+	 }
+	
+	 @Post(':id/stockLog')
+	 async getStockLog(@Body()request  , @Res() res ) : Promise <any>  {
+		 var id = request.id;
+		 var shop_id = request.shop_id;
+		 var title = request.title;
+		 var sub_title = request.sub_title;
+		 var price = request.price;
+		 var quantity = request.quantity;
+		 var category_id = request.category_id;
+		 var sold = request.sold;
+		 var added_date = request.added_date;
+		 var suggest_products = request.suggest_products;
+		 var rating = request.rating;
+		console.log(shop_id)
+		const a = await this.sellerconsoleService.addStock(id ,shop_id,title,sub_title,price,quantity,category_id, sold,new Date (added_date),suggest_products,rating);
+		const b = await this.sellerconsoleService.StockLog(id, shop_id , quantity ,new Date (added_date));
+		console.log(a,b);
+		// res.send(a);
+		res.send(b);
+
+	 }
 }
 
 // @Post()
--- backend/src/sellerconsole/sellerconsole.service.ts
+++ backend/src/sellerconsole/sellerconsole.service.ts
@@ -3,6 +3,7 @@ import { CreateSellerconsoleDto } from './dto/create-sellerconsole.dto';
 import { UpdateSellerconsoleDto } from './dto/update-sellerconsole.dto';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Timestamp } from 'rxjs';
 
 @Injectable()
 export class SellerconsoleService {
@@ -39,6 +40,74 @@ export class SellerconsoleService {
 	async getHistoryDiscount(){
         return await this.prisma.discount.findMany()
     }
+
+	async getStock(id : number){
+		return await this.prisma.product.findMany({
+			where : {
+				shop_id : id,
+			},
+			select : {
+				id : true,
+				title : true,
+				quantity : true,
+				price : true,
+			}
+		})
+	}
+	async addStock(id : number, shopId : number, title : string , sub_title : string ,price : number, quantity : number ,
+						categoryId : number, sold : number , added_date : Date, suggest_product : number[] , rating : number){
+							console.log({
+								id : id,
+								shop_id : shopId,
+								title : title,
+								sub_title : sub_title,
+								price : price,
+								quantity : quantity,
+								category_id : categoryId,
+								sold : sold,
+								added_date : added_date,
+								suggest_products : suggest_product ,
+								rating : rating,
+							})
+		await this.prisma.product.create({
+			data : {
+				shop_id : shopId,
+				title : title,
+				sub_title : sub_title,
+				price : price,
+				quantity : quantity,
+				category_id : categoryId,
+				sold : sold,
+				added_date : added_date,
+				suggest_products : suggest_product ,
+				rating : rating,
+
+			}
+		})
+	}
+	async StockLog(shopId : number, productId : number , quantity : number, added_date : Date){
+	
+		await this.prisma.sconsole_stock_history.create({
+			data : {
+				shop_id : shopId,
+				product_id : productId,
+				quantity : quantity,
+				updated_date : added_date,
+			}
+		})
+		return this.prisma.sconsole_stock_history.findUnique({
+			where : {
+				id : productId ,
+			},
+			select : {
+				shop_id : true,
+				product_id : true,
+				quantity : true,
+				updated_date: true,
+			}
+		})
+	}
+	
 }
 
 // create(createSellerconsoleDto: CreateSellerconsoleDto) {
