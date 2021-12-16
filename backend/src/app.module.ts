1426563a2137b2fe9fe9d25c71384b0c3d6d2817
eatyourhead110 <eatyourhead110@gmail.com>
2021-12-16 14:06:33 +0700

    [UPDATE] init module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/auth.module';
 import { JwtAuthGuard } from './common/guards/jwtAuth.guard';
 import { PrismaModule } from './prisma/prisma.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-	imports: [HttpModule, AuthenticationModule, PrismaModule],
+	imports: [HttpModule, AuthenticationModule, PrismaModule, CartModule],
 	controllers: [AppController],
 	providers: [
 		AppService,
