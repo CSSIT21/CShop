0ced73f0be5bd6afa6bbe2ccaacee30bbc0b0297
Thun <bsthun@gmail.com>
2021-12-17 21:15:39 +0700

    Update scraping 1 (#107)

--- server/scraping-1/src/shopee/search_array.js
+++ server/scraping-1/src/shopee/search_array.js
@@ -2,182 +2,196 @@ const { search } = require('./search_scraper');
 
 const array = async (browser) => {
 	const searchResultComputer = [
-		
-		// * Electronics category
-		
-		// Computer & Laptop
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+		...await mapper(
+			40,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Computers-Laptops-cat.11044958?page=${i}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Beauty-Personal-Care-cat.11012301?filters=5&page=${i}`,
+					{ category: 'beauty' },
+				),
+		),
+		...await mapper(
+			10,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Computers-Laptops-cat.11044958?page=${i + 10}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Furniture-cat.11000001.11001566?filters=5&page=${i}`,
+					{ category: 'furniture' },
+				),
+		),
+		...await mapper(
+			3,
+			3,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Computers-Laptops-cat.11044958?page=${i + 20}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Bathroom-cat.11000001.11011195?filters=5&page=${i}`,
+					{ category: 'furniture' },
+				),
+		),
+		...await mapper(
+			3,
+			3,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Computers-Laptops-cat.11044958?page=${i + 30}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		
-		// Mobile & Gadget
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Beddings-cat.11000001.11011220?filters=5&page=${i}`,
+					{ category: 'furniture' },
+				),
+		),
+		...await mapper(
+			10,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Mobile-Gadgets-cat.11044951?page=${i}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Home-Decor-cat.11000001.11011273?filters=5&page=${i}`,
+					{ category: 'furniture' },
+				),
+		),
+		...await mapper(
+			5,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Mobile-Gadgets-cat.11044951?page=${i + 10}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Home-Organisation-cat.11000001.11011297?filters=5&page=${i}`,
+					{ category: 'furniture' },
+				),
+		),
+		...await mapper(
+			5,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Mobile-Gadgets-cat.11044951?page=${i + 20}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Kitchen-Dining-cat.11000001.11011332?filters=5&page=${i}`,
+					{ category: 'furniture' },
+				),
+		),
+		...await mapper(
+			5,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Mobile-Gadgets-cat.11044951?page=${i + 30}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		
-		// Home Appliances
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Lighting-cat.11000001.11011364?filters=5&page=${i}`,
+					{ category: 'furniture' },
+				),
+		),
+		...await mapper(
+			20,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Home-Appliances-cat.11044955?page=${i}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Home-Appliances-cat.11027421?filters=5&page=${i}`,
+					{ category: 'electronics' },
+				),
+		),
+		...await mapper(
+			5,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Home-Appliances-cat.11044955?page=${i + 10}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Cameras-Drones-cat.11013548?filters=5&page=${i}`,
+					{ category: 'electronics' },
+				),
+		),
+		...await mapper(
+			15,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Home-Appliances-cat.11044955?page=${i + 20}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Food-Beverages-cat.11011871?filters=5&page=${i}`,
+					{ category: 'food' },
+				),
+		),
+		...await mapper(
+			5,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Home-Appliances-cat.11044955?page=${i + 30}`,
-					{ category: 'electronics' }),
-			),
-		)).flat(),
-		
-		// * Education category
-		// Stationary
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Health-Wellness-cat.11027491?filters=5&page=${i}`,
+					{ category: 'food' },
+				),
+		),
+		...await mapper(
+			20,
+			5,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Stationery-Books-Music-cat.11044957?officialMall=true&page=${i}`,
-					{ category: 'education' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Sports-Outdoors-cat.11012018?filters=5&page=${i}`,
+					{ category: 'sports' },
+				),
+		),
+		...await mapper(
+			6,
+			3,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Stationery-Books-Music-cat.11044957?officialMall=true&page=${i + 10}`,
-					{ category: 'education' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Women's-Bags-cat.11012592?filters=5&page=${i}`,
+					{ category: 'accessories' },
+				),
+		),
+		...await mapper(
+			6,
+			3,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Stationery-Books-Music-cat.11044957?officialMall=true&page=${i + 20}`,
-					{ category: 'education' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Men's-Bags-cat.11012659?filters=5&page=${i}`,
+					{ category: 'accessories' },
+				),
+		),
+		...await mapper(
+			9,
+			3,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Stationery-Books-Music-cat.11044957?officialMall=true&page=${i + 30}`,
-					{ category: 'education' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
+					`https://shopee.sg/Jewellery-Accessories-cat.11013077?filters=5&page=${i}`,
+					{ category: 'accessories' },
+				),
+		),
+		...await mapper(
+			6,
+			3,
+			(_, i) =>
+				search(
 					browser,
-					`https://shopee.co.th/Stationery-Books-Music-cat.11044957?officialMall=true&page=${i + 40}`,
-					{ category: 'education' }),
-			),
-		)).flat(),
-		...(await Promise.all(
-			Array.from(
-				Array(10),
-				(_, i) => search(
-					browser,
-					`https://shopee.co.th/Stationery-Books-Music-cat.11044957?officialMall=true&page=${i + 50}`,
-					{ category: 'education' }),
-			),
-		)).flat(),
+					`https://shopee.sg/Watches-cat.11012515?filters=5&page=${i}`,
+					{ category: 'accessories' },
+				),
+		),
 	];
 	
 	return { searchResultComputer };
+};
+
+const mapper = async (page, size, mapfn) => {
+	const results = [];
+	
+	for (const p of Array(page / size).keys()) {
+		results.concat(
+			(await Promise.all(
+				Array.from(
+					Array(10),
+					mapfn,
+				),
+			)).flat(),
+		);
+	}
+	
+	return results;
 };
 
 module.exports = array;
