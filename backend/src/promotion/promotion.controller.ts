52a1587cb87911e3c10696cbf54f430c458f1813
maxky49221 <ratchanon.maxky@mail.kmutt.ac.th>
2021-12-19 04:47:03 +0700

    [UPDATE] promotion

--- backend/src/promotion/promotion.controller.ts
+++ backend/src/promotion/promotion.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Req, Res } from '@nestjs/common';
 import { PromotionService } from './promotion.service';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,14 +7,15 @@ import { response } from 'express';
 import { is } from '@babel/types';
 import { Public } from 'src/common/decorators/public.decorator';
 
+
 const prisma = new PrismaClient();
 
 @Controller('promotion')
 export class PromotionController {
 	constructor(private readonly promotionService: PromotionService, public prisma: PrismaService) {}
-		
 
 	@Post('/upevent')
+	@Public()
 	async AppCouponEvent(@Body()request , @Res() res): Promise<void> {
 		var id = request.discount_id;
 		var userId = request.userId;
@@ -23,13 +24,14 @@ export class PromotionController {
 		const a = await this.promotionService.decodeevent(id);
 		const b = await this.promotionService.userGetCode(userId, id);
 		const c = await this.promotionService.checkcodeUser(userId);
-		console.log(a,b,c);
+		console.log(a,b);
         await res.send(a);
 		await res.send(c);
 
 	}
 
 	@Post('/upapp')
+	@Public()
 	async AppCouponApp(@Req() req, @Res() res): Promise<void> {
 		var request = req.body;
 		var id = request.discount_id;
@@ -44,6 +46,7 @@ export class PromotionController {
 	}
 
 	@Post('/upshop')
+	@Public()
 	async AppCouponShop(@Req() req, @Res() res): Promise<void> {
 		var request = req.body;
 		var id = request.discount_id;
@@ -51,13 +54,14 @@ export class PromotionController {
 		console.log(id)
 		console.log(userId)
 		const a = await this.promotionService.userGetCode(userId, id);
-		const b = await this.promotionService.decodeapp(id)
+		const b = await this.promotionService.decodeshop(id)
 		const c = await this.promotionService.checkcodeUser(userId);
         res.send(b);
 		res.send(c);
 	}
 
 	@Post('/upre')
+	@Public()
 	async AppCouponReward(@Req() req, @Res() res): Promise<void> {
 		var request = req.body;
 		var id = request.discount_id;
@@ -65,13 +69,14 @@ export class PromotionController {
 		console.log(id)
 		console.log(userId)
 		const a = await this.promotionService.userGetCode(userId, id);
-		const b = await this.promotionService.decodeapp(id)
+		const b = await this.promotionService.decodereward(id)
 		const c = await this.promotionService.checkcodeUser(userId);
         res.send(b);
 		res.send(c);
 	}
 
 	@Post('/upcate')
+	@Public()
 	async AppCouponCategory(@Req() req, @Res() res): Promise<void> {
 		var request = req.body;
 		var id = request.discount_id;
@@ -79,13 +84,14 @@ export class PromotionController {
 		console.log(id)
 		console.log(userId)
 		const a = await this.promotionService.userGetCode(userId, id);
-		const b = await this.promotionService.decodeapp(id)
+		const b = await this.promotionService.decodecategory(id)
 		const c = await this.promotionService.checkcodeUser(userId);
         res.send(b);
 		res.send(c);
 	}
 
     @Post('/claim')
+	@Public()
 	async ClaimEvent (@Req() req, @Res() res): Promise<void>{
 		var request = req.body;
 		var userId = request.userId;
