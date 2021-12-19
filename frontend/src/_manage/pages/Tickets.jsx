189255ec5bd72db278d1331233405576a9354047
iceing11 <ice_iceing@hotmail.com>
2021-12-20 03:52:18 +0700

    [UPDATE] update ticket search. I want to sleep

--- frontend/src/_manage/pages/Tickets.jsx
+++ frontend/src/_manage/pages/Tickets.jsx
@@ -161,7 +161,7 @@ const ManageSellerAccountPage = () => {
                     <CardContent sx={{ padding: '15px', paddingBottom: '15px!important'}}>
                     <Box className={classes.header}>
                         <Box sx={{ width: '50%' }} className={classes.header}>
-                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Tickets ({(!showClosed ? (tickets.filter(ticket => ticket.description.toUpperCase().includes(search.toUpperCase())).filter(function( obj ) {return obj.admin_support_status.status.status != 'Closed';})).length : tickets.filter(ticket => ticket.description.toUpperCase().includes(search.toUpperCase())).length)})</Typography>
+                            <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Tickets ({(!showClosed ? (tickets.filter(ticket => ticket.title.toUpperCase().includes(search.toUpperCase())).filter(function( obj ) {return obj.admin_support_status.status.status != 'Closed';})).length : tickets.filter(ticket => ticket.title.toUpperCase().includes(search.toUpperCase())).length)})</Typography>
                         </Box>
                         <Box sx={{ width: '16%' }} className={classes.header}>
                             <Typography style={{ fontWeight: 600, fontSize: '15px' }}>Type</Typography>
@@ -191,8 +191,8 @@ const ManageSellerAccountPage = () => {
                     <CardContent>
                         {
                         (showClosed ? 
-                            (tickets.filter(ticket => ticket.description.toUpperCase().includes(search.toUpperCase())).sort((a,b) => { return (a[sortBy] > b[sortBy]) ? (sortOrder ? -1 : 1) : (sortOrder ? 1 : -1) ; }))
-                            : (tickets.filter(ticket => ticket.description.toUpperCase().includes(search.toUpperCase())).sort((a,b) => { return (a[sortBy] > b[sortBy]) ? (sortOrder ? -1 : 1) : (sortOrder ? 1 : -1) ; }))
+                            (tickets.filter(ticket => ticket.title.toUpperCase().includes(search.toUpperCase())).sort((a,b) => { return (a[sortBy] > b[sortBy]) ? (sortOrder ? -1 : 1) : (sortOrder ? 1 : -1) ; }))
+                            : (tickets.filter(ticket => ticket.title.toUpperCase().includes(search.toUpperCase())).sort((a,b) => { return (a[sortBy] > b[sortBy]) ? (sortOrder ? -1 : 1) : (sortOrder ? 1 : -1) ; }))
                             .filter(function( obj ) {
                                 return obj.admin_support_status.status.status != 'Closed';
                             })
