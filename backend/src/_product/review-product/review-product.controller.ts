ea1710f9bd49e1c35db0bd3fee2f3a0931d05a98
Patiphon Klangpraphan <69670820+cympati@users.noreply.github.com>
2021-12-20 20:51:26 +0700

    Productdetail 2 (#121)
    * [UPDATE] Clear code and Add transition image
    
    * [UPDATE] all
    
    * [INSERT] share and favorite button
    
    * [EDIT] Favorite and Share button
    
    * [ADD] Photo review and report button
    
    * [UPDATE] Comment footer and photo review
    
    * Edit comment
    
    * [Edit] comment
    
    * [UPDATE] comments
    
    * เหนื่อยจัง
    
    * [edit] clear unused code
    
    * Add new structure
    
    * [update] condition
    
    * เพิ่ม+เรียงโครงนิดหน่อย
    
    * Dialog structure
    
    * Almost finish jaaaa
    
    * Product frontend FINISH มั้ง
    
    * [ADD] Conditions
    
    * FINISH product
    
    * [EDIT] color
    
    * ...
    
    * [ADD] backend
    
    * [ADD] backend getProductDetails แต่ต้องรอ database 🥲
    
    * [ADD] frontend product detail
    
    * [UPDATE] backend get comment, product, add short link
    
    * [ADD] shopID
    
    * [UPDATE] review dialog
    
    * [EDIT] review dialog
    
    * [UPDATE] review dialog again
    
    * [ADD] backend get comment pictures
    
    * [UPDATE] get product pictures
    
    * Connect backend
    
    * แก้อะไรไม่รู้จำไม่ได้
    
    * [ADD] createReview
    
    * [UPDATE] backend review pictures
    
    * [UPDATE] backend suggest product
    
    * [ADD] customer id to review page
    
    * [UPDATE] connect frontend and add condition before add to cart
    
    * UPDATE
    
    * backend create shop comment
    
    * edit review dialog นอนดีกว่า
    
    * Edit review product
    
    * almost finish connect frontend
    
    * connect ML
    
    * add to cart api
    
    * add copy link success
    
    * add fav but no finish ka
    
    Co-authored-by: Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
    Co-authored-by: Pw-a02 <praewaphrueksawan2543@gmail.com>
    Co-authored-by: Yok-Thanawan <thanawan.li@mail.kmutt.ac.th>
    Co-authored-by: Thun (Mac Mini) <bsthun@gmail.com>

--- backend/src/_product/review-product/review-product.controller.ts
+++ backend/src/_product/review-product/review-product.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { ReviewProductService } from './review-product.service';
+import { PictureListDto } from './dto/pictureList.dto';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('review-product')
-export class ReviewProductController {}
+export class ReviewProductController {
+	constructor(private readonly reviewProductService: ReviewProductService) {}
+
+	@Post('/:product_id/:customer_id/create-product-review')
+	@Public()
+	async createProductReview(
+		@Param('product_id', ParseIntPipe) product_id: number,
+		@Param('customer_id', ParseIntPipe) customer_id: number,
+		@Body() reviewInput: Prisma.product_reviewsCreateInput,
+		@Body() reviewPictureList: PictureListDto,
+	) {
+		const review = await this.reviewProductService.createProductReview(
+			reviewInput,
+			reviewPictureList,
+			product_id,
+			customer_id,
+		);
+		return {
+			success: true,
+			review,
+		};
+	}
+
+	@Post('/:shop_id/:customer_id/create-shop-review')
+	@Public()
+	async createShopReview(
+		@Param('shop_id', ParseIntPipe) shop_id: number,
+		@Param('customer_id', ParseIntPipe) customer_id: number,
+		@Body() reviewInput: Prisma.shop_commentCreateInput,
+	) {
+		const review = await this.reviewProductService.createShopReview(reviewInput, shop_id, customer_id);
+		return {
+			success: true,
+			review,
+		};
+	}
+}
