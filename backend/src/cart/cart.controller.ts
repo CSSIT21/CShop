a4abf72db913af05794005b89b127d31c3be9faa
Jirasin Jarethammajit <69670650+jirasin02@users.noreply.github.com>
2021-12-21 04:10:12 +0700

    Account 1 (#133)

--- backend/src/cart/cart.controller.ts
+++ backend/src/cart/cart.controller.ts
@@ -6,54 +6,70 @@ import { UpdateCartDto } from './dto/update-cart.dto';
 
 @Controller('cart')
 export class CartController {
-  constructor(private readonly cartService: CartService) {}
+	constructor(private readonly cartService: CartService) {}
 
-  @Post() @Public()
-  create(@Body() createCartDto: CreateCartDto) {
-    return this.cartService.create(createCartDto);
-  }
+	@Post()
+	@Public()
+	create(@Body() createCartDto: CreateCartDto) {
+		return this.cartService.create(createCartDto);
+	}
 
-  @Get() @Public()
-  findAll() {
-    return this.cartService.findAll();
-  }
+	@Get()
+	@Public()
+	findAll() {
+		return this.cartService.findAll();
+	}
 
-  @Get(':id') @Public()
-  findOne(@Param('id') id: string) {
-    return this.cartService.findOne(+id);
-  }
+	@Get(':id')
+	@Public()
+	findOne(@Param('id') id: string) {
+		return this.cartService.findOne(+id);
+	}
 
-  @Patch(':id') @Public()
-  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
-    return this.cartService.update(+id, updateCartDto);
-  }
+	@Patch(':id')
+	@Public()
+	update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
+		return this.cartService.update(+id, updateCartDto);
+	}
 
-  @Post('/addtocart') @Public()
-  addtocart(@Body() request) { 
-    return this.cartService.addtocart(request.userID, request.productID, request.amount, request.firstchoiceID, request.seconedchoiceID);
-  }
+	@Post('/addtocart')
+	@Public()
+	addtocart(@Body() request) {
+		return this.cartService.addtocart(
+			request.userID,
+			request.productID,
+			request.amount,
+			request.firstchoiceID,
+			request.seconedchoiceID,
+		);
+	}
 
-  @Post('/updateamount') @Public()
-  updateamount(@Body() request){
-    return this.cartService.updateamount(request.updateAmount)
-  }
-  @Post('/removefromcart') @Public()
-  removefromcart(@Body() request) { 
-    return this.cartService.removefromcart(request.orderID,request.userID,request.productID);
-  }
+	@Post('/updateamount')
+	@Public()
+	updateamount(@Body() request) {
+		return this.cartService.updateamount(request.updateAmount);
+	}
+	@Post('/removefromcart')
+	@Public()
+	removefromcart(@Body() request) {
+		return this.cartService.removefromcart(request.orderID, request.userID, request.productID);
+	}
 
-  @Post('/removeallfromcart') @Public()
-  removeallfromcart(@Body() request) { 
-    return this.cartService.removeallfromcart(request.userID);
-  }
+	@Post('/removeallfromcart')
+	@Public()
+	removeallfromcart(@Body() request) {
+		return this.cartService.removeallfromcart(request.userID);
+	}
 
-  @Post('/rebuy') @Public()
-  rebuy(@Body() request){
-    return this.cartService.rebuy(request.userID, request.rebuyitem)
-  }
+	@Post('/rebuy')
+	@Public()
+	rebuy(@Body() request) {
+		return this.cartService.rebuy(request.userID, request.cartitems);
+	}
 
-  @Delete(':id') @Public()
-  remove(@Param('id') id: string) {
-    return this.cartService.remove(+id);
-  }
+	@Delete(':id')
+	@Public()
+	remove(@Param('id') id: string) {
+		return this.cartService.remove(+id);
+	}
 }
--- frontend/src/_home/pages/Favourite.jsx
+++ frontend/src/_home/pages/Favourite.jsx
@@ -1,93 +1,100 @@
 import { useState, useEffect } from "react";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import HeaderWithIcon from "../components/commonBase/HeaderWithIcon";
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 import { Box } from "@mui/system";
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 import CardAndPagination from "../components/commonBase/CardAndPagination";
 import axios from "axios";
 import config from "~/common/constants";
 import { useRecoilValue } from "recoil";
 import authState from "~/common/store/authState";
 
-
 const FavouritePage = () => {
-    const classes = useStyles();
-    const { isLoggedIn, user } = useRecoilValue(authState);
-    const [skip, setSkip] = useState(0);
-    const [page, setPage] = useState(1);
-    const [products, setProducts] = useState([]);
-    const [count, setCount] = useState(1);
+  const classes = useStyles();
+  const { isLoggedIn, user } = useRecoilValue(authState);
+  const [skip, setSkip] = useState(0);
+  const [page, setPage] = useState(1);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(1);
 
-    const onFavorite = () => {
+  const onFavorite = () => {};
 
-    };
+  const onPageChange = (e, value) => {
+    setPage(value);
+  };
 
-    const onPageChange = (e, value) => {
-        setPage(value)
-    };
+  const getData = () => {
+    if (isLoggedIn) {
+      setSkip((page - 1) * 16);
+      axios
+        .get(
+          `${config.SERVER_URL}/home/favorites/${user.id}?take=16&skip=${skip}`
+        )
+        .then(({ data }) => {
+          if (data.success) {
+            setCount(data.favorites.count);
+            return setProducts(data.favorites.products);
+          } else {
+            return console.log(data);
+          }
+        })
+        .catch((err) => {
+          return console.log(err.message);
+        });
+    }
+  };
 
-    const getData = () => {
-        if (isLoggedIn) {
-            setSkip((page - 1) * 16);
-            axios
-                .get(`${config.SERVER_URL}/home/favorites/${user.id}?take=16&skip=${skip}`)
-                .then(({ data }) => {
-                    if (data.success) {
-                        setCount(data.favorites.count);
-                        return setProducts(data.favorites.products);
-                    }
-                    else {
-                        return console.log(data);
-                    }
-                })
-                .catch((err) => {
-                    return console.log(err.message);
-                })
-        }
-    };
+  useEffect(() => {
+    getData();
+  }, [page]);
 
-    useEffect(() => {
-        getData();
-    }, [page])
-
-    return (
-        <Box className={classes.FavoriteWrapper}>
-            <Box className={classes.content}>
-                <HeaderWithIcon title="Wish list" ItemIcon={FavoriteIcon} />
-                {products.length > 0
-                    ? (<CardAndPagination products={products} onPageChange={onPageChange} count={count} page={page} onFavorite={onFavorite} />)
-                    : (<Typography
-                        textAlign="center"
-                        fontSize={16}
-                        fontWeight={400}
-                        color="gray">
-                        No products in your wish list
-                    </Typography>)}
-            </Box>
-        </Box>
-    );
+  return (
+    <Box className={classes.FavoriteWrapper}>
+      <Box className={classes.content}>
+        <HeaderWithIcon title="Wish list" ItemIcon={FavoriteIcon} />
+        {products.length > 0 ? (
+          <CardAndPagination
+            products={products}
+            onPageChange={onPageChange}
+            count={count}
+            page={page}
+            onFavorite={onFavorite}
+          />
+        ) : (
+          <Typography
+            textAlign="center"
+            fontSize={16}
+            fontWeight={400}
+            color="gray"
+          >
+            No products in your wish list
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  );
 };
 
 const useStyles = makeStyles({
-    FavoriteWrapper: {
-        width: '100%',
-        minHeight: '100vh',
+  FavoriteWrapper: {
+    width: "100%",
+    minHeight: "100vh",
 
-        display: 'flex',
-        justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
 
-        backgroundColor: '#FDF4DD',
-    },
-    content: {
-        maxWidth: '1100px',
-        padding: '50px 0',
+    backgroundColor: "#FDF4DD",
+  },
+  content: {
+    maxWidth: "1100px",
+    padding: "50px 0",
 
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default FavouritePage;
