b71bc1199d64df44428b0ec8356607c689a1c92b
Thun (Mac Mini) <bsthun@gmail.com>
2021-12-22 11:09:40 +0700

    Revert "[UPDATE]"
    This reverts commit 60123fe6f7da5a8de350bbcfe3baa1c8b33620e7.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/auth.module';
 import { JwtAuthGuard } from './common/guards/jwtAuth.guard';
 import { PrismaModule } from './prisma/prisma.module';
-//import { ChatModule } from './chat/chat.module';
+import { ChatModule } from './chat/chat.module';
 import { PromotionModule } from './promotion/promotion.module';
 import { ProfileModule } from './profile/profile.module';
 import { SellershopModule } from './_sellershop/sellershop/sellershop.module';
@@ -24,7 +24,6 @@ import { CommentModule } from './_product/comment/comment.module';
 import { SearchModule } from './_search/search.module';
 import { DeliveryModule } from './delivery/delivery.module';
 import { ManageaccountModule } from './_manageaccount/manageaccount.module';
-import { PaymentModule } from './_payment/payment.module';
 
 @Module({
 	imports: [
@@ -46,8 +45,7 @@ import { PaymentModule } from './_payment/payment.module';
 		ReviewProductModule,
 		ShortLinkModule,
 		CommentModule,
-		PaymentModule,
-		//ChatModule,
+		ChatModule,
 		PromotionModule,
 		SearchModule,
 		DeliveryModule,
